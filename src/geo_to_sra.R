cf6b351839aa54978a989136d222ebc529649760
David McGaughey <mcgaughey@gmail.com>
2019-06-27 21:25:40 -0400

    succesfully pulls geo id and sra project ids

--- src/geo_to_sra.R
+++ src/geo_to_sra.R
@@ -12,30 +12,46 @@ base_url <- 'https://www.ncbi.nlm.nih.gov/gds/?term='
 GEO_ids <- scan('data/GEO_IDs.txt', what = 'character')
 search_url <- paste0(base_url, paste(GEO_ids, collapse = '+'))
 
-
 gse_prj <- data.frame(matrix(ncol = 2, nrow = 0))
+# extract GEO ID and SRA project ID from web page
 colnames(gse_prj) <- c("GSE", "SRA_PROJECT_ID")
 for (i in GEO_ids){
   search_url <- paste0(base_url, i)
   page <- GET(search_url) %>% content(., 'text', )
-  split <- (str_split(page, pattern = ' |\\=|\\"|\\[|\\]|\\<|\\>'))[[1]]
-  gse <- split[grepl('GSE',split)] %>% unique() %>% head(1)
-  prj <- split[grepl('PRJNA',split)] %>% unique() %>% head(1)
+  split <- (str_split(page, pattern = ' '))[[1]]
+  acc <- split[grepl('acc', split)]
+  acc <- (str_split(acc, pattern = '\\=|\\"|\\[|\\]|\\<|\\>')) %>% unlist()
+  gse <- acc[grepl('GSE', acc)] %>% unique() 
+  prj <- acc[grepl('PRJNA', acc)] %>% unique() 
   if (length(prj) == 0){prj <- NA}
   line = c(gse, prj)
   names(line) <- c('GSE', 'SRA_PROJECT_ID')
-
   gse_prj <- bind_rows(gse_prj, line)
 }
-page <- GET(search_url) %>% content(., 'text', )
-split <- str_split(page, pattern = ' |\\=|\\"|\\[|\\]|\\<|\\>')
-  # get SRA accessions / info from @seandavi12 omicidx 
-  # billing is the project name in the Google
-  # bigQuery web UI
-  con <- dbConnect(bigrquery::bigquery(), 
-                   project = 'isb-cgc-01-0006', 
-                   dataset = 'omicdx', billing = 
-                     'rosy-solstice-244919')
+# if SRA ID is missing, then load specific GEO page from GSE
+# and search for it
+base_url <- 'https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc='
+missing_sra_gse <- gse_prj %>% filter(is.na(SRA_PROJECT_ID)) %>% pull(GSE)
+gse_prj2 <- data.frame(matrix(ncol = 2, nrow = 0))
+# extract GEO ID and SRA project ID from web page
+colnames(gse_prj2) <- c("GSE", "SRA_PROJECT_ID")
+for (i in missing_sra_gse){
+  search_url <- paste0(base_url, i)
+  page <- GET(search_url) %>% content(., 'text', )
+  split <- (str_split(page, pattern = ' '))[[1]]
+  acc <- split[grepl('acc', split)]
+  acc <- (str_split(acc, pattern = '\\=|\\"|\\[|\\]|\\<|\\>')) %>% unlist()
+  gse <- i
+  prj <- acc[grepl('PRJNA', acc)] %>% unique() 
+  if (length(prj) == 0){prj <- NA}
+  line = c(gse, paste(prj, collapse = ','))
+  names(line) <- c('GSE', 'SRA_PROJECT_ID')
+  gse_prj2 <- bind_rows(gse_prj2, line)
+}
+
+library(GEOquery)
+x <- getGEO('GSE12601')
+
 
 # do not select all columns, as that costs more money
 # bigquery essentially charges by data searched
