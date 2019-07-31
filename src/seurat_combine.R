84b5c6e2f1b5dd52ae27fd0c0eb473bfd28be0cd
David McGaughey <mcgaughey@gmail.com>
2019-07-31 15:04:15 -0400

    trying to rework to rpca based method, as CCT using too much mem at integratedata

--- src/seurat_combine.R
+++ src/seurat_combine.R
@@ -23,17 +23,31 @@ for (i in seq(1,length(rdata_files))){
   sc_data[[sample_accession]] <- seurat_obj
 }
 
+
+
 sc_data_features <- SelectIntegrationFeatures(object.list = sc_data, nfeatures = 3000, verbose = FALSE)
 
+sc_data <- lapply(X = sc_data, FUN = function(x) {
+   # x <- ScaleData(x, features = sc_data_features, verbose = FALSE)
+    x <- RunPCA(x, features = sc_data_features, verbose = FALSE)
+})
 sc_data <- PrepSCTIntegration(object.list = sc_data, anchor.features = sc_data_features, verbose = FALSE)
 # this one takes HOURS
-sc_data_anchors <- FindIntegrationAnchors(object.list = sc_data, normalization.method = 'SCT', anchor.features = sc_data_features, verbose = TRUE)
+#sc_data_anchors <- FindIntegrationAnchors(object.list = sc_data, normalization.method = 'SCT', anchor.features = sc_data_features, verbose = TRUE)
 
-sc_data_integrated <- IntegrateData(anchorset = sc_data_anchors, normalization.method = 'SCT', verbose = TRUE)
+
+# nope, trying rpca now, running out of memory
+anchors <- FindIntegrationAnchors(object.list = sc_data, normalization.method = 'SCT', scale = FALSE, anchor.features = sc_data_features, reduction = "rpca")
+
+sc_data_integrated <- IntegrateData(anchorset = anchors, normalization.method = 'SCT', verbose = TRUE)
 
 # PCA, UMAP
 sc_data_integrated <- RunPCA(sc_data_integrated, npcs = 100, ndims.print = 1:5, nfeatures.print = 5)
 sc_data_integrated <- RunUMAP(sc_data_integrated, dims = 1:75, min.dist = 0.75)
+
+pdf('merged.pdf')
+print(DimPlot(sc_data_integrated, group.by = 'orig.ident'))
+dev.off()
 
 save(sc_data_integrated, file = args[1])
 
