94ff961fea4b10e40bf77cc3dfc3c9e9f293059c
David McGaughey <mcgaughey@gmail.com>
2019-08-02 16:39:51 -0400

    updated with suggestions from butler

--- src/seurat_combine.R
+++ src/seurat_combine.R
@@ -20,30 +20,33 @@ for (i in seq(1,length(rdata_files))){
   file = rdata_files[i]
   sample_accession = str_extract(file, '(SRS|iPSC_RPE_scRNA_)\\d+')
   load(file)
-  sc_data[[sample_accession]] <- seurat_obj
+  #sc_data[[sample_accession]] <- SCTransform(seurat_obj)
+  sc_data[[sample_accession]] <- RunPCA(seurat_obj)
 }
 
 
 
 sc_data_features <- SelectIntegrationFeatures(object.list = sc_data, nfeatures = 3000, verbose = FALSE)
 
-sc_data <- lapply(X = sc_data, FUN = function(x) {
-   # x <- ScaleData(x, features = sc_data_features, verbose = FALSE)
-    x <- RunPCA(x, features = sc_data_features, verbose = FALSE)
-})
 sc_data <- PrepSCTIntegration(object.list = sc_data, anchor.features = sc_data_features, verbose = FALSE)
 # this one takes HOURS
-#sc_data_anchors <- FindIntegrationAnchors(object.list = sc_data, normalization.method = 'SCT', anchor.features = sc_data_features, verbose = TRUE)
+sc_data_anchors <- FindIntegrationAnchors(object.list = sc_data, 
+		normalization.method = 'SCT', 
+		reduction = "rpca", 
+		anchor.features = sc_data_features, 
+		verbose = TRUE)
 
+# get one sample per study / age
+#ref_samples <- sra_metadata_extended %>% filter(library_layout == 'PAIRED', UMI == 'YES', organism == 'Mus musculus') %>% group_by(study_accession, Age) %>% sample_n(2, replace = TRUE) %>% pull(sample_accession) %>% unique() 
 
 # nope, trying rpca now, running out of memory
-anchors <- FindIntegrationAnchors(object.list = sc_data, normalization.method = 'SCT', scale = FALSE, anchor.features = sc_data_features, reduction = "rpca")
+#anchors <- FindIntegrationAnchors(object.list = sc_data, normalization.method = 'SCT', scale = FALSE, anchor.features = sc_data_features, reduction = "rpca")
 
 sc_data_integrated <- IntegrateData(anchorset = anchors, normalization.method = 'SCT', verbose = TRUE)
 
 # PCA, UMAP
-sc_data_integrated <- RunPCA(sc_data_integrated, npcs = 100, ndims.print = 1:5, nfeatures.print = 5)
-sc_data_integrated <- RunUMAP(sc_data_integrated, dims = 1:75, min.dist = 0.75)
+sc_data_integrated <- RunPCA(sc_data_integrated, npcs = 200, ndims.print = 1:5, nfeatures.print = 5)
+sc_data_integrated <- RunUMAP(sc_data_integrated, dims = 1:20, min.dist = 0.75)
 
 pdf('merged.pdf')
 print(DimPlot(sc_data_integrated, group.by = 'orig.ident'))
