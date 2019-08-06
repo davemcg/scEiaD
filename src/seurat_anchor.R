757c2a5c3ddc91e87a41d3f1200c512cdaa4f6d9
David McGaughey <mcgaughey@gmail.com>
2019-08-06 09:56:08 -0400

    splitting seurat anchor integrate into two scripts

---
+++ src/seurat_anchor.R
@@ -0,0 +1,95 @@
+# combine datasets by species
+
+# Use SCTransform to normalize the data, then RPCA to integrate
+args <- commandArgs(trailingOnly = TRUE)
+library(Seurat)
+library(Matrix)
+library(tidyverse)
+library(future)
+plan(strategy = "multicore", workers = 12)
+# the first term is roughly the number of MB of RAM you expect to use
+# 40000 ~ 40GB
+options(future.globals.maxSize = 40000 * 1024^2)
+
+# load in metadata for study project merging, UMI correction, and gene name changing
+metadata <- read_tsv(args[2])
+tx <- read_tsv(args[3], col_names = FALSE) %>% select(2,3) %>% unique()
+colnames(tx) <- c('id', 'gene')
+
+# well data
+load(args[4])
+
+# sparse matrix files
+rdata_files = args[5:length(args)]
+
+# roll through UMI data, 
+# correct gene names (upper case), force to be unique
+# add sample ID to UMI
+# e.g. AAATATAAAA_SRS2341234
+sc_data <- list()
+droplet_samples <- list()
+for (i in seq(1,length(rdata_files))){
+  file = rdata_files[i]
+  sample_accession = str_extract(file, '(SRS|iPSC_RPE_scRNA_)\\d+')
+  droplet_samples <- c(droplet_samples, sample_accession)
+  load(file)
+  row.names(res_matrix) <- row.names(res_matrix) %>% 
+    enframe(value = 'id') %>% 
+    left_join(., tx, by = 'id') %>% 
+    pull(gene) %>% 
+    toupper()
+  colnames(res_matrix) <- make.unique(colnames(res_matrix))
+  colnames(res_matrix) <- paste0(colnames(res_matrix), "_", sample_accession)
+  row.names(res_matrix) <- make.unique(row.names(res_matrix))
+  sc_data[[sample_accession]] <- res_matrix
+}
+
+# grab available well samples
+well_samples <- colnames(tpm)
+
+# merge by project
+study_sample <- metadata %>% 
+  filter(sample_accession %in% c(droplet_samples, well_samples)) %>% 
+  select(study_accession, sample_accession) %>% 
+  unique() %>% 
+  mutate(tech = case_when(sample_accession %in% droplet_samples ~ 'droplet',
+                          TRUE ~ 'well')) %>% 
+  arrange(study_accession)
+
+# merge droplet and well data into one list of sparse matrices by study_accession
+study_data <- list()
+for (i in unique(study_sample %>% pull(study_accession))){
+  print(i)
+  samples <- study_sample %>% filter(study_accession == i) %>% pull(sample_accession) 
+  tech <- study_sample %>% filter(study_accession == i) %>% pull(tech) %>% head(1)
+  if (tech == 'droplet'){
+  study_data[[i]] <- do.call(cbind, sc_data[samples])
+  } else {
+    # remove samples that aren't in tpm (which means they failed upstream QC)
+    samples <- samples[samples %in% well_samples]
+    study_data[[i]] <- tpm[,samples]
+    row.names(study_data[[i]]) <- row.names(study_data[[i]]) %>% toupper()
+    row.names(study_data[[i]]) <- make.unique(row.names(study_data[[i]]))
+    # remove na columns
+    study_data[[i]] <- study_data[[i]][,colSums(is.na(study_data[[i]])) < 1]
+  }
+  study_data[[i]] <- CreateSeuratObject(study_data[[i]], project = i)
+  study_data[[i]] <- SCTransform(study_data[[i]])
+  study_data[[i]] <- RunPCA(study_data[[i]])
+}
+
+study_data_features <- SelectIntegrationFeatures(object.list = study_data, nfeatures = 3000, verbose = FALSE)
+study_data <- PrepSCTIntegration(object.list = study_data, anchor.features = study_data_features, verbose = FALSE)
+
+# have to apply PCA after SelectIntegrationFeatures and PrepSCTIntegration
+study_data <- lapply(X = study_data, FUN = function(x) {
+  x <- RunPCA(x, features = study_data_features, verbose = FALSE)
+})
+
+# nope, trying rpca now, running out of memory with the "CCT" reduction method
+anchors <- FindIntegrationAnchors(object.list = study_data, 
+                                  normalization.method = 'SCT', 
+                                  scale = FALSE, 
+                                  anchor.features = study_data_features, 
+                                  reduction = "rpca")
+
