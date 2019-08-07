63c606ed0013cb0327ae133b4ec889970b7b484e
David McGaughey <mcgaughey@gmail.com>
2019-08-07 15:52:59 -0400

    improve age labelling, add tissueNote to metadata for snakemake (blank_combine) use

--- src/id_tissue_and_age.R
+++ src/id_tissue_and_age.R
@@ -16,9 +16,9 @@ sra_metadata_extended <- sra_metadata %>%
   mutate(SRP158081_age = str_extract(biosample_title,'^[E|P]\\d+')) %>% 
   mutate(SRP158081_age = case_when(grepl('^E', SRP158081_age) ~ substr(SRP158081_age, 2,6) %>% as.numeric() - 19,
                                    grepl('^P', SRP158081_age) ~ substr(SRP158081_age, 2,6) %>% as.numeric,
-                                   TRUE ~ 100000)) %>% 
+                                   TRUE ~ 1000)) %>% 
   mutate(SRP161678_age = case_when(study_accession == 'SRP161678' ~ str_extract(biosample_attribute_recs, 'Day\\s\\d+') %>% gsub('Day ','',.) %>% as.numeric(),
-                                   TRUE ~ 100000)) %>% 
+                                   TRUE ~ 1000)) %>% 
   mutate(Tissue = case_when(study_accession == 'SRP050054' ~ 'Retina',
                             study_accession == 'SRP073242' ~ 'Retina',
                             study_accession == 'SRP075719' ~ 'Retina',
@@ -42,14 +42,15 @@ sra_metadata_extended <- sra_metadata %>%
                          study_accession == 'SRP075719' ~ 17,
                          study_accession == 'SRP075720' ~ 17,
                          study_accession == 'SRP106476' ~ 90,
-                         study_accession == 'SRP157927' ~ 100000,
+                         study_accession == 'SRP157927' ~ 1000,
                          study_accession == 'SRP158081' ~ SRP158081_age,
-                         study_accession == 'SRP158528' ~ 2000,
+                         study_accession == 'SRP158528' ~ 1000,
                          study_accession == 'SRP159286' ~ 240,
                          study_accession == 'SRP161678' ~ SRP161678_age,
-                         study_accession == 'SRP166660' ~ 100000,
-                         study_accession == 'SRP186407' ~ 100000,
-                         study_accession == 'SRP194595' ~ 100000,
+                         study_accession == 'SRP166660' ~ 1000,
+                         study_accession == 'SRP186407' ~ 1000,
+                         study_accession == 'SRP194595' ~ 1000,
+                         study_accession == 'SRP136739' ~ 90,
                          TRUE ~ 999999),   
          TissueNote = case_when(study_accession == 'SRP073242' ~ 'Vsx2-GFP FACS',
                                 study_accession == 'SRP075720' ~ 'Kcng4-cre;stop-YFP X Thy1-stop-YFP Line#1',
@@ -58,6 +59,8 @@ sra_metadata_extended <- sra_metadata %>%
                                 study_accession == 'SRP157927' & grepl('M2', biosample_title) ~ 'Macaque 2, Fovea',
                                 study_accession == 'SRP157927' & grepl('M3', biosample_title) ~ 'Macaque 3, Fovea',
                                 study_accession == 'SRP157927' & grepl('M4', biosample_title) ~ 'Macaque 4, Fovea',
+                                study_accession == 'SRP194595' & grepl('Fovea', biosample_title) ~ 'Fovea',
+                                study_accession == 'SRP194595' & grepl('Peripheral', biosample_title) ~ 'Peripheral',
                                 study_accession == 'SRP158081' ~ 'Sorted Retinal Progenitor Cell (Chx10-GFP positive)',
                                 study_accession == 'SRP158528' ~ biosample_attribute_recs %>% gsub('retina ', '', .),
                                 study_accession == 'SRP159286' ~ 'Retinal organoid',
@@ -65,7 +68,8 @@ sra_metadata_extended <- sra_metadata %>%
                                 study_accession == 'SRP166660' ~ 'Retina, Adult Glast-CreERT2+/tg; Yap5SA+/tg; R26R-tdTomato+/tg mice were induced with tamoxifen followed by NMDA injection. After 2 days, tdTomato+ cells were isolated by fluorescence-activated cell sorting (FACS)',
                                 sample_accession == 'SRS4386076' ~ 'Cx3cr1+ single cells from the mouse model of light-induced retinal degeneration',
                                 sample_accession == 'SRS4386075' ~ 'Cx3cr1+ single cells, control',
-                                sample_accession == 'SRP194595' ~ str_extract(biosample_attribute_recs, 'Retina.*\\d+') %>% gsub("Retina, ","",.)))
+                                sample_accession == 'SRP194595' ~ str_extract(biosample_attribute_recs, 'Retina.*\\d+') %>% gsub("Retina, ","",.))) %>% 
+  dplyr::select(-SRP158081_age, -SRP161678_age)
 
 save(sra_metadata_extended, file = 'data/sra_metadata_extended.Rdata')
 
---
+++ src/srs_srr_layout_organism_tech.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+load('data/sra_metadata_extended.Rdata')
+# hand add our internal RPE samples
+core_rpe = data.frame(sample_accession = c(rep('iPSC_RPE_scRNA_01', 24), rep('iPSC_RPE_scRNA_02', 24), rep('iPSC_RPE_scRNA_03', 24)),
+                      run_accession = c('scRNA_01_S1_L001_HL7H3BCX2','scRNA_01_S1_L001_HLCLYBCX2','scRNA_01_S1_L001_HLFW7BCX2','scRNA_01_S1_L002_HL7H3BCX2','scRNA_01_S1_L002_HLCLYBCX2','scRNA_01_S1_L002_HLFW7BCX2','scRNA_01_S2_L001_HL7H3BCX2','scRNA_01_S2_L001_HLCLYBCX2','scRNA_01_S2_L001_HLFW7BCX2','scRNA_01_S2_L002_HL7H3BCX2','scRNA_01_S2_L002_HLCLYBCX2','scRNA_01_S2_L002_HLFW7BCX2','scRNA_01_S3_L001_HL7H3BCX2','scRNA_01_S3_L001_HLCLYBCX2','scRNA_01_S3_L001_HLFW7BCX2','scRNA_01_S3_L002_HL7H3BCX2','scRNA_01_S3_L002_HLCLYBCX2','scRNA_01_S3_L002_HLFW7BCX2','scRNA_01_S4_L001_HL7H3BCX2','scRNA_01_S4_L001_HLCLYBCX2','scRNA_01_S4_L001_HLFW7BCX2','scRNA_01_S4_L002_HL7H3BCX2','scRNA_01_S4_L002_HLCLYBCX2','scRNA_01_S4_L002_HLFW7BCX2','scRNA_02_S5_L001_HL7H3BCX2','scRNA_02_S5_L001_HLCLYBCX2','scRNA_02_S5_L001_HLFW7BCX2','scRNA_02_S5_L002_HL7H3BCX2','scRNA_02_S5_L002_HLCLYBCX2','scRNA_02_S5_L002_HLFW7BCX2','scRNA_02_S6_L001_HL7H3BCX2','scRNA_02_S6_L001_HLCLYBCX2','scRNA_02_S6_L001_HLFW7BCX2','scRNA_02_S6_L002_HL7H3BCX2','scRNA_02_S6_L002_HLCLYBCX2','scRNA_02_S6_L002_HLFW7BCX2','scRNA_02_S7_L001_HL7H3BCX2','scRNA_02_S7_L001_HLCLYBCX2','scRNA_02_S7_L001_HLFW7BCX2','scRNA_02_S7_L002_HL7H3BCX2','scRNA_02_S7_L002_HLCLYBCX2','scRNA_02_S7_L002_HLFW7BCX2','scRNA_02_S8_L001_HL7H3BCX2','scRNA_02_S8_L001_HLCLYBCX2','scRNA_02_S8_L001_HLFW7BCX2','scRNA_02_S8_L002_HL7H3BCX2','scRNA_02_S8_L002_HLCLYBCX2','scRNA_02_S8_L002_HLFW7BCX2','scRNA_03_S10_L001_HL7H3BCX2','scRNA_03_S10_L001_HLCLYBCX2','scRNA_03_S10_L001_HLFW7BCX2','scRNA_03_S10_L002_HL7H3BCX2','scRNA_03_S10_L002_HLCLYBCX2','scRNA_03_S10_L002_HLFW7BCX2','scRNA_03_S11_L001_HL7H3BCX2','scRNA_03_S11_L001_HLCLYBCX2','scRNA_03_S11_L001_HLFW7BCX2','scRNA_03_S11_L002_HL7H3BCX2','scRNA_03_S11_L002_HLCLYBCX2','scRNA_03_S11_L002_HLFW7BCX2','scRNA_03_S12_L001_HL7H3BCX2','scRNA_03_S12_L001_HLCLYBCX2','scRNA_03_S12_L001_HLFW7BCX2','scRNA_03_S12_L002_HL7H3BCX2','scRNA_03_S12_L002_HLCLYBCX2','scRNA_03_S12_L002_HLFW7BCX2','scRNA_03_S9_L001_HL7H3BCX2','scRNA_03_S9_L001_HLCLYBCX2','scRNA_03_S9_L001_HLFW7BCX2','scRNA_03_S9_L002_HL7H3BCX2','scRNA_03_S9_L002_HLCLYBCX2','scRNA_03_S9_L002_HLFW7BCX2'),
+                      library_layout = rep('PAIRED', 72),
+                      organism = rep('Homo sapiens', 72),
+                      Platform = rep('10xv2', 72),
+                      UMI = rep('YES', 72),
+                      study_accession = rep('OGVFB_Hufnagel_iPSC_RPE', 72),
+                      TissueNote = c(rep('iPSC', 24), rep('RPE day 42', 24), rep('RPE Transwell',24)),
+                      stringsAsFactors = FALSE)
+
+# remove BULK RNA-seq and SRP149898 which is missing the crucial paired end reads (need to contact author)
+write_tsv(bind_rows(sra_metadata_extended %>% 
+                      select(sample_accession, run_accession, library_layout, organism, Platform, UMI, study_accession, TissueNote) %>% 
+                      filter(Platform != 'BULK', 
+                             study_accession != 'SRP149898') %>% 
+                      unique(), 
+                    core_rpe),
+          path = 'data/sample_run_layout_organism_tech.tsv')
+write_tsv(sra_metadata %>% group_by(organism, Platform) %>% sample_n(1) %>% select(sample_accession, run_accession, library_layout, organism, Platform, UMI), path = 'data/sample_run_layout_organism_tech_for_svg.tsv')
+
