c4f59a4a6b0eb1aed42e9aedc2b4f7d56af31a54
David McGaughey <mcgaughey@gmail.com>
2019-09-19 11:38:15 -0400

    update logic

--- src/calculate_umap_and_cluster.R
+++ src/calculate_umap_and_cluster.R
@@ -3,7 +3,11 @@ library(Seurat)
 
 args <- commandArgs(trailingOnly = TRUE)
 method = args[1]
+# cell labels
 load(args[2])
+# integrated_obj
+load(args[3])
+
 
 create_umap_and_cluster <- function(integrated_obj, 
                                   max_dims = 20, 
@@ -29,27 +33,41 @@ create_umap_and_cluster <- function(integrated_obj,
 }
 
 if (method == 'CCA'){
+  reduction_name_key <- 'ccaUMAP_'
   integrated_obj <- create_umap_and_cluster(integrated_obj, 
                                           20,
                                           'pca',
-                                          reduction_name_key = 'ccaUMAP_')
+                                          reduction_name_key = reduction_name_key)
 } else if (method == 'scanorama'){
+  reduction_name_key <- 'scanoramaUMAP_'
   integrated_obj <- create_umap_and_cluster(integrated_obj, 
                                           20,
                                           'scanorama',
-                                          reduction_name_key = 'scanoramaUMAP_')
+                                          reduction_name_key = reduction_name_key)
 } else if (method == 'harmony'){
+  reduction_name_key <- 'harmonyUMAP_'
   integrated_obj <- create_umap_and_cluster(integrated_obj, 
                                           20,
                                           'harmony',
-                                          reduction_name_key = 'harmonyUMAP_')
+                                          reduction_name_key = reduction_name_key)
 } else if (method = 'fastMNN'){
+  reduction_name_key <- 'mnnUMAP_'
   integrated_obj <- create_umap_and_cluster(integrated_obj, 
                                           20,
                                           'mnn',
-                                          reduction_name_key = 'mnnUMAP_')
+                                          reduction_name_key = reduction_name_key)
 } else {
   print(paste0("Why did you pick ", method, "?"))
 }
 
-save(integrated_obj, file = args[3], compress = FALSE )
+save(integrated_obj, file = args[4], compress = FALSE )
+
+# left_join known cell labels
+orig_meta <- integrated_obj@meta.data %>% as_tibble(rownames = 'Barcode')
+umap <- Embeddings(integrated_obj[[reduction_name_key]]) %>% as_tibble(rownames = 'Barcode') %>% 
+  left_join(., orig_meta) %>% 
+  left_join(., meta %>% dplyr::rename(Barcode = value))
+umap$Method <- method
+colnames(umap)[2:3] <- c('UMAP_1', 'UMAP_2')
+
+save(umap, file = args[5])
