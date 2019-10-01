03be73abbdda5774968f74926795b2d776e1c2bd
David McGaughey <mcgaughey@gmail.com>
2019-10-01 11:09:14 -0400

    update logic for handling missing well data

--- src/build_sparse_matrix.R
+++ src/build_sparse_matrix.R
@@ -4,27 +4,24 @@
 
 args <- commandArgs(trailingOnly = TRUE)
 
-#args <- c('seurat_obj/Mus_musculus__standard_and_SCT__late__batch.seuratV3.Rdata','late','batch','/home/mcgaugheyd/git/massive_integrated_eye_scRNA/data/sample_run_layout_organism_tech.tsv','references/gencode.vM22.metadata.MGI_tx_mapping.tsv','quant/Mus_musculus/counts.Rdata','quant/SRS866911/genecount/matrix.Rdata','quant/SRS866908/genecount/matrix.Rdata','quant/SRS1467254/genecount/matrix.Rdata','quant/SRS3971245/genecount/matrix.Rdata','quant/SRS3971246/genecount/matrix.Rdata','quant/SRS4363764/genecount/matrix.Rdata','quant/SRS1467251/genecount/matrix.Rdata','quant/SRS1467253/genecount/matrix.Rdata','quant/SRS3674976/genecount/matrix.Rdata','quant/SRS3674982/genecount/matrix.Rdata','quant/SRS3674983/genecount/matrix.Rdata','quant/SRS4363765/genecount/matrix.Rdata','quant/SRS3674974/genecount/matrix.Rdata','quant/SRS3674975/genecount/matrix.Rdata','quant/SRS3674985/genecount/matrix.Rdata','quant/SRS1467249/genecount/matrix.Rdata','quant/SRS3674980/genecount/matrix.Rdata','quant/SRS3971244/genecount/matrix.Rdata','quant/SRS4363763/genecount/matrix.Rdata','quant/SRS4386076/genecount/matrix.Rdata','quant/SRS1467250/genecount/matrix.Rdata','quant/SRS3674978/genecount/matrix.Rdata','quant/SRS3674977/genecount/matrix.Rdata','quant/SRS3674988/genecount/matrix.Rdata','quant/SRS3674979/genecount/matrix.Rdata','quant/SRS3674981/genecount/matrix.Rdata','quant/SRS3674984/genecount/matrix.Rdata','quant/SRS4386075/genecount/matrix.Rdata','quant/SRS1467252/genecount/matrix.Rdata','quant/SRS3674987/genecount/matrix.Rdata','quant/SRS866912/genecount/matrix.Rdata','quant/SRS866910/genecount/matrix.Rdata','quant/SRS866909/genecount/matrix.Rdata','quant/SRS866907/genecount/matrix.Rdata','quant/SRS4363762/genecount/matrix.Rdata','quant/SRS866906/genecount/matrix.Rdata')
-
 library(Matrix)
 library(tidyverse)
 library(Seurat)
+species <- str_split(args[1], '/')[[1]][2]
 
-metadata <- read_tsv(args[3])
-tx <- read_tsv(args[4], col_names = FALSE) %>% select(2,3) %>% unique()
+metadata <- read_tsv(args[4])
+tx <- read_tsv(args[5], col_names = FALSE) %>% select(2,3) %>% unique()
 colnames(tx) <- c('id', 'gene')
 
 # well data
-load(args[5])
-# # remove cells with > 10000 or < 1000 detected genes
-# count <- count[,(diff(count@p) < 10000)]
-# count <- count[,(diff(count@p) > 1000)]
-
-# extract species
-species <- str_split(args[6], '/')[[1]][2]
-
-# sparse matrix files
-rdata_files = args[8:length(args)]
+if (species != "Macaca_fascicularis"){
+  load(args[5]) # well data
+  rdata_files = args[6:length(args)]
+  ## make row names for count (well) upper case
+  row.names(count) <- toupper(row.names(count))
+} else {
+  rdata_files = args[5:length(args)]
+}
 
 # roll through UMI data, 
 # correct gene names (upper case), force to be unique
@@ -50,11 +47,14 @@ for (i in seq(1,length(rdata_files))){
   sc_data[[sample_accession]] <- res_matrix
 }
 
-# create naive fully merged for 
-## make row names for count (well) upper case
-row.names(count) <- toupper(row.names(count))
+# create naive fully merged  
 droplet <- Reduce(cbind, sc_data) 
-m <- Matrix.utils::merge.Matrix(count, droplet, by.x=row.names(count), by.y = row.names(droplet))
+if (species != "Macaca_fascicularis"){
+  m <- Matrix.utils::merge.Matrix(count, droplet, by.x=row.names(count), by.y = row.names(droplet))
+} else {
+  m <- droplet 
+}
+
 m <- m[row.names(m) != 'fill.x', ] 
 # create sample table
 cell_info <- colnames(m) %>% enframe() %>% 
