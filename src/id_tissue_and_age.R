f128ddbc67cf22c272531ba884fc24e1b518d77e
David McGaughey <mcgaughey@gmail.com>
2019-10-11 16:22:56 -0400

    update metadata with new samples

--- src/id_tissue_and_age.R
+++ src/id_tissue_and_age.R
@@ -14,7 +14,9 @@ for (row in 1:nrow(sra_metadata)){
       spread(attribute_name, value)
   } else {attribute_info[[row]] <- tibble(age = NA)}
 }
-biosample_attribute_recs <- attribute_info %>% bind_rows()
+biosample_attribute_recs <- attribute_info %>% 
+  bind_rows() %>% 
+  unite(biosample_attribute_recs, remove = FALSE, sep = ' ')
 # biosample_attribute_recs <- sra_metadata %>% 
 #   select(run_accession, biosample_attribute_recs) %>% 
 #   unnest() %>% 
@@ -24,34 +26,20 @@ biosample_attribute_recs <- attribute_info %>% bind_rows()
 sra_metadata_extended <- sra_metadata %>% select(-biosample_attribute_recs)
 sra_metadata_extended <- cbind(sra_metadata_extended, biosample_attribute_recs)
 
-sra_metadata_extended <- sra_metadata %>% 
+sra_metadata_extended <- sra_metadata_extended %>% 
   filter(Platform != 'BULK', study_accession != 'SRP149898') %>% 
-
   mutate(SRP158081_age = str_extract(biosample_title,'^[E|P]\\d+')) %>% 
-  mutate(SRP158081_age = case_when(grepl('^E', SRP158081_age) ~ substr(SRP158081_age, 2,6) %>% as.numeric() - 19,
+  mutate(SRP158081_age = case_when(grepl('Nfia', biosample_title) ~ 14,
+                                   grepl('^E', SRP158081_age) ~ substr(SRP158081_age, 2,6) %>% as.numeric() - 19,
                                    grepl('^P', SRP158081_age) ~ substr(SRP158081_age, 2,6) %>% as.numeric,
-                                   biosample_attribute_recs == 'Retinal Cells P14 Mixed CD1/C57Bl6' ~ 14,
-                                   TRUE ~ 1000)) %>% 
-  mutate(SRP161678_age = case_when(study_accession == 'SRP161678' ~ str_extract(biosample_attribute_recs, 'Day\\s\\d+') %>% gsub('Day ','',.) %>% as.numeric(),
-                                   TRUE ~ 1000)) %>% 
-  mutate(Tissue = case_when(study_accession == 'SRP050054' ~ 'Retina',
-                            study_accession == 'SRP073242' ~ 'Retina',
-                            study_accession == 'SRP075719' ~ 'Retina',
-                            study_accession == 'SRP075720' ~ 'Retina',
-                            study_accession == 'SRP106476' & !grepl('hPSC', biosample_attribute_recs) ~ 'Retina',
-                            study_accession == 'SRP106476' & grepl('hPSC', biosample_attribute_recs) ~ 'Retina',
-                            study_accession == 'SRP136739' ~ 'Retina',
-                            study_accession == 'SRP157927' ~ 'Retina',
-                            study_accession == 'SRP158081' ~ 'Retina',
-                            study_accession == 'SRP158528' ~ 'Retina',
-                            study_accession == 'SRP159286' ~ 'Retina',
-                            study_accession == 'SRP161678' ~ 'Retina',
-                            study_accession == 'SRP166660' ~ 'Retina',
-                            study_accession == 'SRP186407' ~ 'Retina',
-                            study_accession == 'SRP194595' ~ 'Retina'),
+                                   biosample_attribute_recs == 'Retinal Cells P14 Mixed CD1/C57Bl6' ~ 14)) %>% 
+  mutate(SRP161678_age = case_when(study_accession == 'SRP161678' ~ str_extract(`time point`, 'Day\\s\\d+') %>% 
+                                     gsub('Day ','',.) %>% as.numeric())) %>% 
+  mutate(Tissue = 'Retina',
          Source = case_when(study_accession == 'SRP106476' & grepl('hPSC', biosample_attribute_recs) ~ 'hiPSC',
                             study_accession %in% c('SRP136739', 'SRP159286', 'SRP161678') ~ 'hiPSC',
-                            TRUE ~ 'Tissue'),  
+                            study_accession == 'SRP125998' ~ 'Embryonic',
+                            study_accession == 'SRP170038' ~ 'Organoid'),  
          Age = case_when(study_accession == 'SRP050054' ~ 14,
                          study_accession == 'SRP073242' ~ 17,
                          study_accession == 'SRP075719' ~ 17,
@@ -66,7 +54,10 @@ sra_metadata_extended <- sra_metadata %>%
                          study_accession == 'SRP186407' ~ 1000,
                          study_accession == 'SRP194595' ~ 1000,
                          study_accession == 'SRP136739' ~ 90,
-                         TRUE ~ 999999),   
+                         study_accession == 'SRP125998' ~ (str_extract(`developmental stage`, '\\d') %>% as.numeric()) * 7,
+                         study_accession == 'SRP186396' ~ 19 - (str_extract(`developmental stage`, '\\d+') %>% as.numeric()),
+                         study_accession == 'SRP158081' ~ 14,
+                         study_accession %in% c('SRP200599','SRP168426') ~ 19 - str_extract(`developmental stage`, '\\d') %>% as.numeric()),   
          Covariate = case_when((study_accession == 'SRP157927' | study_accession == 'SRP158528') & grepl('M1', biosample_title) ~ 'Macaque1',
                                (study_accession == 'SRP157927' | study_accession == 'SRP158528') & grepl('M2', biosample_title) ~ 'Macaque2',
                                (study_accession == 'SRP157927' | study_accession == 'SRP158528') & grepl('M3', biosample_title) ~ 'Macaque3',
@@ -74,14 +65,17 @@ sra_metadata_extended <- sra_metadata %>%
                                study_accession == 'SRP075719' & grepl('Batch 1', title) ~ 'Batch1',
                                study_accession == 'SRP075719' & grepl('Batch 2', title) ~ 'Batch2',
                                study_accession == 'SRP050054' ~ str_extract(title, 'retina\\s\\d') %>% gsub(' ', '', .),
-                               study_accession == 'SRP075720' & grepl('P17 1', biosample_attribute_recs) ~ 'Batch1',
-                               study_accession == 'SRP075720' & grepl('P17 2', biosample_attribute_recs) ~ 'Batch2',
-                               study_accession == 'SRP194595' ~  gsub('\\s(Periphery|Fovea)\\sDonor\\s','',biosample_attribute_recs),
+                               study_accession == 'SRP075720' & grepl('1', `retina id`) ~ 'Batch1',
+                               study_accession == 'SRP075720' & grepl('2', `retina id`) ~ 'Batch2',
+                               study_accession == 'SRP194595' ~  donor,
                                study_accession == 'SRP075719' ~ sample_accession,
                                study_accession == 'SRP158081' & grepl('rep2', biosample_title) ~ 'Rep2',
                                study_accession == 'SRP158081' & grepl('rep3', biosample_title) ~ 'Rep3',
                                study_accession == 'SRP158081' ~ 'Rep1',
-                               TRUE ~ 'None'),  
+                               study_accession == 'SRP125998' ~ str_extract(title, 'rep\\d'),
+                               study_accession == 'SRP168426' ~ str_extract(title, 'E2|F2'),
+                               study_accession == 'SRP166660' ~ str_extract(biosample_title, 'run\\d'),
+                               study_accession  %in% c('SRP222001','SRP222958') ~ str_extract(title, 'retina \\d') %>% gsub(' ', '', .)),  
          TissueNote = case_when(study_accession == 'SRP158528' | study_accession == 'SRP157927' ~ biosample_title,
                                 study_accession == 'SRP073242' ~ 'Vsx2-GFP FACS',
                                 study_accession == 'SRP075720' ~ 'Kcng4-cre;stop-YFP X Thy1-stop-YFP Line#1',
@@ -93,14 +87,16 @@ sra_metadata_extended <- sra_metadata %>%
                                 study_accession == 'SRP194595' & grepl('Fovea', biosample_title) ~ 'Fovea',
                                 study_accession == 'SRP194595' & grepl('Peripheral', biosample_title) ~ 'Peripheral',
                                 study_accession == 'SRP158081' ~ 'Sorted Retinal Progenitor Cell (Chx10-GFP positive)',
-                                study_accession == 'SRP158528' ~ biosample_attribute_recs %>% gsub('retina ', '', .),
+                                study_accession == 'SRP158528' ~ paste0(source_name, ', ', tissue, ', ', sorting),
                                 study_accession == 'SRP159286' ~ 'Retinal organoid',
                                 study_accession == 'SRP161678' ~ 'Retinal organoid, human ESC (CRX-GFP line)',
                                 study_accession == 'SRP166660' ~ 'Retina, Adult Glast-CreERT2+/tg; Yap5SA+/tg; R26R-tdTomato+/tg mice were induced with tamoxifen followed by NMDA injection. After 2 days, tdTomato+ cells were isolated by fluorescence-activated cell sorting (FACS)',
                                 sample_accession == 'SRS4386076' ~ 'Cx3cr1+ single cells from the mouse model of light-induced retinal degeneration',
                                 sample_accession == 'SRS4386075' ~ 'Cx3cr1+ single cells, control',
-                                sample_accession == 'SRP194595' ~ str_extract(biosample_attribute_recs, 'Retina.*\\d+') %>% gsub("Retina, ","",.))) %>% 
+                                sample_accession == 'SRP194595' ~ location,
+                                study_accession  %in% c('SRP222001','SRP222958') ~ str_extract(title, 'Macula retina \\d|Peripheral retina \\d'))) %>% 
   dplyr::select(-SRP158081_age, -SRP161678_age)
+
 
 save(sra_metadata_extended, file = 'data/sra_metadata_extended.Rdata')
 
