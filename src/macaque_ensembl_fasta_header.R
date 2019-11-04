7db048e2560dd5861e4e3e23bf422045e78d1f9b
David McGaughey <mcgaughey@gmail.com>
2019-11-04 15:58:23 -0500

    fix mistakes

--- src/macaque_ensembl_fasta_header.R
+++ src/macaque_ensembl_fasta_header.R
@@ -2,9 +2,11 @@ args <- commandArgs(trailingOnly = TRUE)
 library(tidyverse)
 
 headers <-read_lines(args[1])
-ensmfat <- str_split(x, ' ') %>% map(1) %>% unlist()
-gene <- str_split(x, ' ') %>% map(4) %>% unlist() %>% gsub('gene:', '', .)
-symbol <- x %>% str_extract(., 'gene_symbol:\\S*') %>% gsub('gene_symbol:','',.)
+ensmfat <- str_split(headers, ' ') %>% map(1) %>% unlist()
+gene <- str_split(headers, ' ') %>% map(4) %>% unlist() %>% gsub('gene:', '', .)
+symbol <- headers %>% str_extract(., 'gene_symbol:\\S*') %>% gsub('gene_symbol:','',.)
 
-out <- cbind(ensmfat, gene, symbol) %>% as_tibble()
+out <- cbind(ensmfat, gene, symbol) %>% as_tibble() %>% 
+  mutate(symbol = case_when(is.na(symbol) ~ gene,
+                            TRUE ~ symbol))
 write_tsv(out, path = args[2], col_names = FALSE)
