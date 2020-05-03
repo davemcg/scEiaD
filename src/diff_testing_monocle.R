bbb6892484dd3b71982f1d8eebf8abbac96ce234
David McGaughey <mcgaughey@gmail.com>
2020-05-03 13:19:18 -0400

    alter arg input

--- src/diff_testing_monocle.R
+++ src/diff_testing_monocle.R
@@ -5,15 +5,15 @@ library(tidyverse)
 #load('umap/Mus_musculus_Macaca_fascicularis_Homo_sapiens__n_features2000__counts__full__batch__scVI__dims30__mindist0.1__nneighbors100.umap.Rdata')
 
 args <- commandArgs(trailingOnly = TRUE)
-load(args[1]) # seurat cluster obj
-load(args[2]) # monocle obj
+#load(args[1]) # seurat cluster obj
+load(args[1]) # monocle obj
 #cds_retina@colData$monocleCluster <- cds_retina@clusters$UMAP$clusters %>% as.factor()
-cds_retina@colData$seuratCluster <- meta[,2] %>% pull(1) %>%  as.factor()
-cds_retina@colData$seuratSubCluster <- meta[,3] %>% pull(1) %>%  as.factor()
+#cds_retina@colData$seuratCluster <- meta[,2] %>% pull(1) %>%  as.factor()
+#cds_retina@colData$seuratSubCluster <- meta[,3] %>% pull(1) %>%  as.factor()
 
-piece_n <- as.numeric(args[3]) # number of pieces to subset into
-the_n <- as.numeric(args[4]) # which subset this run is doing
-model <- args[5] #e.g "cluster-batch-percent.mt" where cluster is the comparison of interest and the remainder are covariates
+piece_n <- as.numeric(args[2]) # number of pieces to subset into
+the_n <- as.numeric(args[3]) # which subset this run is doing
+model <- args[4] #e.g "cluster-batch-percent.mt" where cluster is the comparison of interest and the remainder are covariates
 if (model == 'A'){
   model <- '~seuratCluster+batch+percent.mt'
 } else if (model == 'B') {
@@ -24,7 +24,7 @@ if (model == 'A'){
   model <- '~CellType_predict+batch+percent.mt+organism'
 } 
 
-output <- args[6]
+output <- args[5]
 
 chunks <- split(1:nrow(cds_retina), 
                 sort(rep_len(1:piece_n, 
