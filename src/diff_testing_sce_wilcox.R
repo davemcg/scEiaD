b9990c354a5668eb15d6f50cde1b5d361bff10e9
David McGaughey <mcgaughey@gmail.com>
2020-05-03 13:19:36 -0400

    summarize step

--- src/diff_testing_sce_wilcox.R
+++ src/diff_testing_sce_wilcox.R
@@ -37,4 +37,20 @@ markers_wilcox <- findMarkers(int_sce,
 				min.prop = 0.8,
 				BPPARAM=MulticoreParam(as.integer(args[5])))
 
-save(markers_wilcox, file = args[6]) 
+
+
+markers_summary <- list()
+for (i in names(markers_wilcox)){
+  print(i)
+  temp <- markers_wilcox[[i]][,4:ncol(markers_wilcox[[i]])] %>% as.matrix()
+  count <-  apply(temp, 1, function(x) sum(x > 0.7))
+  med_auc <- apply(temp, 1, function(x) median(x))
+  mean_auc <- apply(temp, 1, function(x) mean(x))
+  gene <- row.names(temp)
+  cluster <- i
+  markers_summary[[i]] <- cbind(gene, count, med_auc, mean_auc, cluster) %>% as_tibble()
+}
+markers_summary <- markers_summary %>% bind_rows() %>% mutate(count = as.numeric(count), med_auc = as.numeric(med_auc), mean_auc = as.numeric(mean_auc))
+
+save(markers_wilcox, file = args[6])
+save(markers_summary, file = args[7])
