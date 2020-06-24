b7314a975c4165cefc213cd81842be59b247b1ec
David McGaughey <mcgaughey@gmail.com>
2020-06-24 15:02:27 -0400

    handling for differnt norm methods

--- src/run_desc.py
+++ src/run_desc.py
@@ -7,13 +7,20 @@ import scanpy.api as sc
 args = sys.argv
 
 adata = sc.read_loom(args[1], sparse = True)
+type = args[2]
+latent = int(args[3])
+if type == 'counts':
+	sc.pp.normalize_total(adata, target_sum=1e4)
+	sc.pp.log1p(adata)
+	adata.raw = adata
+	desc.scale(adata, zero_center = True, max_value = 3)
+#desc.normalize_per_cell(adata, counts_per_cell_after=1e4)
+#desc.log1p(adata)
+#adata.raw = adata
+#desc.scale(adata, zero_center = True, max_value = 3)
 
-desc.normalize_per_cell(adata, counts_per_cell_after=1e4)
-desc.log1p(adata)
-adata.raw = adata
-desc.scale(adata, zero_center = True, max_value = 3)
-
-adata_train = desc.train(adata, 
+adata_train = desc.train(adata,
+				   dims=[adata.shape[1], 128, 8], n_neighbors=10,
 				   tol=0.1,
                    save_dir="desc_result", 
 				   do_tsne=False, 
