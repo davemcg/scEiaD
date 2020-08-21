6d7a881a88c854cd76b2a3264e6f83257407d232
David McGaughey <mcgaughey@gmail.com>
2020-08-21 11:51:42 -0400

    turning the graph into a matrix with umap set to the number of latent dims

--- src/run_bbknn.py
+++ src/run_bbknn.py
@@ -7,9 +7,9 @@ import scanpy as sc
 args = sys.argv
 latent = int(args[2])
 adata = sc.read_loom(args[1], sparse = True)
-sc.tl.pca(adata, latent)
+sc.pp.pca(adata, latent)
 bb.bbknn(adata, batch_key = 'batch')
+sc.tl.umap(adata, n_components = latent)
 
-
-obsm_data=pd.DataFrame(adata.varm['PCs'])
+obsm_data=pd.DataFrame(adata.obsm['X_umap'])
 obsm_data.to_csv(args[1] + ".csv", sep=",")
