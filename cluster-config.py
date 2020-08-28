278287f2785f8a7a338e099809ef59ec9d1f422f
Vinay Siddhartha Swamy <swamyvinny@gmail.com>
2020-08-28 17:09:28 -0400

    new way to submit jobs to cluster - allows for specificying pwildcard level cluster configuration

---
+++ cluster-config.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+#%% 
+import os
+import sys
+import json
+import pickle 
+from snakemake.utils import read_job_properties
+#%%
+cluster_json_file = sys.argv[1]
+jobscript = sys.argv[2]
+custom_config_rules = ['integrate_00']
+#%%
+
+#%%
+with open(cluster_json_file) as j:
+    cluster_json = json.load(j)
+#%%
+
+params = cluster_json['__default__']
+job_properties = read_job_properties(jobscript)
+params['cpus-per-task'] = job_properties['threads']# we are setting job properties within the snakefile
+#%%
+
+
+rule = job_properties['rule']
+if rule in cluster_json:
+    for key in cluster_json[rule]:
+        params[key] = cluster_json[rule][key]
+elif rule in custom_config_rules:
+    # specifify custom configureations specific rules
+    if rule == 'integrate_00':
+        params['partition']='norm'
+        if job_properties['wildcards']['method'] == 'scVI':
+            params['extra'] = '--gres=gpu:v100x:1,lscratch:5'
+            params['time'] = '24:00:00'
+            params['mem'] = '200G'
+        elif job_properties['wildcards']['method'] == 'CCA':
+            params['partition']='largemem'
+            params['time']='96:00:00'
+            params['mem']='500G'
+            params['time']='96:00:00'
+            params['extra'] = '--gres=lscratch:5'
+        else:
+            params['mem']='350G'
+            params['time']='24:00:00'
+            params['extra'] = '--gres=lscratch:5'
+else:# use default parameters
+    params = cluster_json['__default__'] 
+
+#%%
+
+#fill in the command 
+sbcmd=f'''sbatch --cpus-per-task={params['cpus-per-task']} \
+    --mem={params['mem']} \
+    --time={params['time']} \
+    --partition={params['partition']} \
+    --output={params['output']} \
+    --error={params['error']} \
+    {params['extra']} \
+    {jobscript}
+
+'''
+
+os.system(sbcmd)
+
+# %%
