0638159f09bc6da7969659d0513e4900d8278f2a
Vinay Siddhartha Swamy <swamyvinny@gmail.com>
2020-09-14 12:58:55 -0400

    fixed the geneid to gene name problem

--- src/build_sparse_matrix.R
+++ src/build_sparse_matrix.R
@@ -3,62 +3,82 @@
 # scanorama, CCT, harmony, liger
 
 args <- commandArgs(trailingOnly = TRUE)
-
+save(args, file = 'testing/build_sp_mtx_hm.rdata')
 library(Matrix)
 library(tidyverse)
 library(Seurat)
+library(Matrix)
+library(Matrix.utils)
+
 species <- args[1]
-
+cell_info_file <- args[2]
+final_sparse_matrix_file <- args[3]
 metadata <- read_tsv(args[4])
-tx <- read_tsv(args[5], col_names = FALSE) %>% select(2,3) %>% unique()
-colnames(tx) <- c('id', 'gene')
+gtf <-  rtracklayer::readGFF(args[5])
+args_noidx = args[-(1:5)]
+well_samples <-args_noidx[grepl('__counts.Rdata', args_noidx)]
+droplet_samples <- args_noidx[grepl('matrix.Rdata', args_noidx)]
+#tx <- read_tsv(args[5], col_names = FALSE) %>% select(2,3) %>% unique()
+#colnames(tx) <- c('id', 'gene')
 
-# well data
+
+load_rdata = function(fl){
+  load(fl)
+  if ('count' %in% ls()){
+    return(count)  
+  }else if('res_matrix' %in% ls()){
+    return(res_matrix)
+  }
+  
+}
+
+
 print(species)
 if (species != "Macaca_fascicularis"){
-  load(args[6]) # well data
-  rdata_files = args[7:length(args)]
+
+  all_well_data <- lapply(well_samples, load_rdata) %>% reduce(RowMergeSparseMatrices) 
   ## make row names for count (well) upper case
-  row.names(count) <- toupper(row.names(count))
-} else {
-  rdata_files = args[6:length(args)]
-}
+} 
 
 # roll through UMI data, 
 # correct gene names (upper case), force to be unique
 # add sample ID to UMI
 # e.g. AAATATAAAA_SRS2341234
 sc_data <- list()
-droplet_samples <- list()
-for (i in seq(1,length(rdata_files))){
-  file = rdata_files[i]
-  sample_accession = str_extract(file, '(ERS|SRS|iPSC_RPE_scRNA_)\\d+')
-  droplet_samples <- c(droplet_samples, sample_accession)
-  load(file)
+droplet_sample_accessions <- list()
+empty_droplets <- list()
+
+for (sample in droplet_samples ){
+  drops <- load_rdata(sample)
+  sample_accession = str_extract(sample, '(ERS|SRS|iPSC_RPE_scRNA_)\\d+')
+  if (is.null(dim(drops))){
+    empty_droplets <- c(empty_droplets, sample_accession)
+  } else{
+  
+  droplet_sample_accessions<- c(droplet_sample_accessions, sample_accession)
+
   #if (species != "Macaca_fascicularis") {
-  row.names(res_matrix) <- row.names(res_matrix) %>% 
-    enframe(value = 'id') %>% 
-    left_join(., tx, by = 'id') %>% 
-    pull(gene) %>% 
-    toupper()
+  row.names(drops) <- row.names(drops) %>% str_remove_all('\\.$')
+    
   # } 
-  colnames(res_matrix) <- make.unique(colnames(res_matrix))
-  colnames(res_matrix) <- paste0(colnames(res_matrix), "_", sample_accession)
-  row.names(res_matrix) <- make.unique(row.names(res_matrix))
-  sc_data[[sample_accession]] <- res_matrix
+  colnames(drops) <- make.unique(colnames(drops))
+  colnames(drops) <- paste0(colnames(drops), "_", sample_accession)
+  row.names(drops) <- make.unique(row.names(drops))
+  sc_data[[sample_accession]] <- drops
+  }
 }
 
 # create naive fully merged  
-droplet <- Reduce(cbind, sc_data) 
+all_droplet_data <- reduce(sc_data, RowMergeSparseMatrices)
 if (species != "Macaca_fascicularis"){
-  m <- Matrix.utils::merge.Matrix(count, droplet, by.x=row.names(count), by.y = row.names(droplet))
+  all_data <- RowMergeSparseMatrices(all_droplet_data, all_well_data)
 } else {
-  m <- droplet 
+  all_data <- all_droplet_data
 }
 
-m <- m[row.names(m) != 'fill.x', ] 
+all_data  <- all_data [row.names(all_data ) != 'fill.x', ] 
 # create sample table
-cell_info <- colnames(m) %>% enframe() %>% 
+cell_info <- colnames(all_data ) %>% enframe() %>% 
   mutate(sample_accession = str_extract(value, '(ERS|SRS|iPSC_RPE_scRNA_)\\d+')) %>% 
   left_join(metadata %>% select(-run_accession) %>% unique()) %>% 
   data.frame()
@@ -67,7 +87,34 @@ row.names(cell_info) <- cell_info$value
 cell_info <- cell_info %>% mutate(batch = paste(study_accession, Platform, Covariate, sep = '_'),
                                   batch2 = paste(study_accession, Covariate, sep = '_'),
                                   batch3 = paste(Platform, Covariate, sep = '_'))
+####VS:
+# when generating alignment indices with BUSparse, geneid's are used; for well data, I could summarrise to gene name when all nonUMI is merged
+# but felt that it was better to treat all data the same. multiple geneids map to the same gene_name so aggregate by gene name 
+####
+geneid2gene_name <- gtf %>% filter(type == 'transcript') %>% select(gene_id, gene_name) %>% distinct
+gene_name_ordered_group <- row.names(all_data) %>% 
+  {tibble(gene_id = .)} %>% 
+  inner_join(geneid2gene_name) %>% 
+  pull(gene_name) %>% toupper()
+all_data_by_genename <- aggregate.Matrix(x = all_data, groupings = gene_name_ordered_group, fun = 'sum')
+
 #cell_info <- cell_info %>% mutate(Age = case_when(Age > 100 ~ 30, TRUE ~ Age))
 # save barcodes for labelling with published cell type assignment 
-write_tsv(cell_info, path = args[2])
-save(m, file = args[3], compress = FALSE)
+write_tsv(cell_info, path = cell_info_file)
+save(all_data_by_genename , file = final_sparse_matrix_file, compress = FALSE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/merge_nonUMI_quant_by_organism.R
+++ src/merge_nonUMI_quant_by_organism.R
@@ -1,22 +1,29 @@
 #!/usr/bin/env Rscript
 args = commandArgs(trailingOnly=TRUE)
-
+save(args, file = 'testing/spnew_mnumi_arg.Rdata')
 library(tximport)
 library(readr)
 library(stringr)
 library(dplyr)
 library(Matrix)
+files <- args[seq(5,length(args))]
 
-files <- args[seq(4,length(args))]
+tx2gene <- read_tsv(args[3], col_names = c('transcript_id_dot', 'gene_id_dot')) %>% 
+  mutate(gene_id = str_remove(gene_id_dot, '\\.$'), 
+         transcript_id = str_remove(transcript_id_dot, '\\.$') )
+gtf <- rtracklayer::readGFF(args[4]) %>% as_tibble 
+geneid2gene_name <- gtf %>% filter(type == "transcript") %>% select(gene_id, gene_name) %>% distinct
+tx2gene <- tx2gene %>% 
+  left_join( geneid2gene_name) %>% 
+  mutate(gene_name = replace(gene_name, is.na(gene_name), gene_id[is.na(gene_name)]))
+#save(tx2gene, file = args[3])
 
-tx2gene <- read_tsv(args[3], col_names = F) %>% select(X1, X3)
+SRS = str_split(files, 'quant/') %>% sapply(function(x) x[2]) %>% str_split('/') %>% sapply(function(x) x[1])
 
-SRS = str_extract(files, 'SRS.*/') %>% gsub('/','',.)
-
-txi_count <- tximport(files, type = 'kallisto', tx2gene = tx2gene %>% select(X1, X3), countsFromAbundance = 'no')
-
+txi_count <- tximport(files, type = 'kallisto', tx2gene = tx2gene %>% select(transcript_id_dot, gene_id), countsFromAbundance = 'no')
 count <- txi_count$counts
 colnames(count) <- SRS
+rownames(count) <- str_remove(rownames(count), '\\.$') 
 count <- count %>% Matrix(., sparse = TRUE)
 
 save(count, file = args[1])
