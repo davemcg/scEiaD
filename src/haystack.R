c8fe658d171c0fd70aa4096a48967a55f69f2ac1
David McGaughey <mcgaughey@gmail.com>
2020-09-15 08:18:53 -0400

    update to newer haystack, remove apply

--- src/haystack.R
+++ src/haystack.R
@@ -2,17 +2,30 @@ library(tidyverse)
 library(singleCellHaystack)
 library(Seurat)
 library(Matrix)
-
+library(tictoc)
 args = commandArgs(trailingOnly=TRUE)
 load(args[1])
 num_cell <- as.integer(args[2])
 set.seed(1253)
-cells <- sample(1:ncol(integrated_obj@assays$RNA@counts), num_cell)
-detect <- as.matrix( integrated_obj@assays$RNA@counts[,cells] > 0)
-scvi <- Embeddings(integrated_obj, reduction = 'scVI')[cells,]
-cts <- as.matrix( integrated_obj@assays$RNA@counts[,cells])
-gd <- apply(detect, 2, sum)
+#if (is.na(num_cell)){num_cell = ncol(integrated_obj@assays$RNA@counts)}
+#cells <- sample(1:ncol(integrated_obj@assays$RNA@counts), num_cell)
+#detect <- ( integrated_obj@assays$RNA@counts[,cells] > 0)
+
+if (!is.na(num_cell)){
+	cells <- sample(1:ncol(integrated_obj@assays$RNA@counts), num_cell)
+	cts <- integrated_obj@assays$RNA@counts[,cells]
+	detect <- cts > 0
+	gd <- colSums(detect)
+	scvi <- Embeddings(integrated_obj, reduction = 'scVI')[cells,]
+} else {
+	detect <- ( integrated_obj@assays$RNA@counts > 0)
+	scvi <- Embeddings(integrated_obj, reduction = 'scVI')#[cells,]
+	cts <- ( integrated_obj@assays$RNA@counts) #[,cells])
+	gd <- colSums(detect)
+}
 rm(integrated_obj)
+tic()
 scH <- haystack(x = scvi , detection = detect, use.advanced.sampling = gd)
+toc()
 
 save(scH, file = args[3])
