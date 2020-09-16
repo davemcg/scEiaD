580d8d2e8c81f7d0b44362616c89cd130370f136
David McGaughey <mcgaughey@gmail.com>
2020-09-16 13:08:57 -0400

    custom handling for scArches cluster edge situation

--- src/extract_cluster.R
+++ src/extract_cluster.R
@@ -7,5 +7,9 @@ load(args[1])
 
 meta <- integrated_obj@meta.data %>% as_tibble(rownames = 'Barcode') %>% select(Barcode, contains('cluster'))
 graph <- integrated_obj@misc[[names(integrated_obj@misc)]]
+# swap scArches subcluster for cluster as default params get SO FEW DAMN CLUSTERS 
+if (grepl('scArches', args[1])){
+	meta <- meta[,c(1,3,2)]
+}
 save(meta, file = args[2])
 save(graph, file = args[3])
