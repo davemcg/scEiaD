ae6a126757b8648bf190320d5d58d1c9496cdff7
David McGaughey <mcgaughey@gmail.com>
2020-09-16 13:10:41 -0400

    remove less relevant cell types

--- src/trajectory_slingshot.R
+++ src/trajectory_slingshot.R
@@ -16,26 +16,36 @@ integrated_obj@meta.data$cluster <- meta[,2] %>% pull(1)
 
 orig_meta <- integrated_obj@meta.data %>% as_tibble(rownames = 'Barcode')
 
-umap <- Embeddings(integrated_obj[[reduction]]) %>% as_tibble(rownames = 'Barcode')  %>% 
-  left_join(., integrated_obj@meta.data %>% as_tibble(rownames = 'Barcode') %>% select(Barcode, cluster))
+umapX <- Embeddings(integrated_obj[[reduction]]) %>% as_tibble(rownames = 'Barcode')  %>% 
+  	left_join(., orig_meta %>% select(Barcode, cluster)) %>% 
+	left_join(., umap %>% select(-cluster)) 
+umap <- umapX
+# only retain retina populations as this seems to improve trajectory building
+# makes sense as non-retina populations don't derive from RPCs....
+# also retina unknown as they may be intermediate cell types?
+umapX <- umapX %>% filter(grepl('Precu|RPC|Ganglio|Amacrine|Bipolar|Rods|Cones|Muller|Neurog|Horizon', CellType_predict) | is.na(CellType_predict) )
+umapX$CellType_predict[is.na(umapX$CellType_predict)] <- 'Unknown' 
 
 
+# now cutdown to a lower n so this finishes in less than a week
+umapX <- umapX %>% sample_n(num_cells)
 # Got: 
 # Error in solve.default(s1 + s2) : system is computationally singular: reciprocal condition number = 1.16521e-18
 # trying to add some noise as per: https://github.com/kstreet13/slingshot/issues/35
 #sce <- Seurat::as.SingleCellExperiment(integrated_obj)
-sce <- SingleCellExperiment(list(counts = integrated_obj@assays$RNA@counts[,umap$Barcode]),
-                                 colData= umap %>% select(cluster))
+sce <- SingleCellExperiment(list(counts = integrated_obj@assays$RNA@counts[,umapX$Barcode]),
+                                 colData= umapX %>% select(cluster))
 
 
-reducedDims(sce) <- list(scVI= umap %>% select(contains(reduction)) )
+reducedDims(sce) <- list(scVI= umapX %>% select(contains(reduction)) )
 
 
 set.seed(2534)
-sds_in <- apply(reducedDim(sce, "scVI"), 2, function(x) jitter(x))
+sds_in <- apply(reducedDim(sce, reduction), 2, function(x) jitter(x))
 
 sds <- slingshot(sds_in, 
                  clusterLabels = sce$cluster, 
                  start.clus = start_clus, 
-                 stretch = 0, approx_points = 10000)
-save(sds, sds_in, file = args[6], compress = FALSE)
+                 stretch = 0, approx_points = 1000)
+
+save(sds, sds_in, umap, umapX, file = out, compress = FALSE)
