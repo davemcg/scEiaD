e725b91f5f53f36aa09c6551a965d108f8084ca6
David McGaughey <mcgaughey@gmail.com>
2020-09-22 10:03:17 -0400

    update wrapper to use vs fancy py job config req and fix rule flow in SnakePOP

--- SnakePOP.wrapper.sh
+++ SnakePOP.wrapper.sh
@@ -4,27 +4,21 @@
 # module load snakemake || exit 1
 
 # activate conda
-source /data/mcgaugheyd/conda/etc/profile.d/conda.sh
-conda activate kallisto_046
+rm -rf 00log/
+mkdir -p 00log/ 
+snakefile=$1
+config=$2
+cluster_json=$3
+cluster_config=$4
+conda_sh=$5
+# activate conda
+source ${conda_sh}
 
-mkdir -p 00log 
-sbcmd="sbatch --cpus-per-task={threads} \
---mem={cluster.mem} \
---time={cluster.time} \
---partition={cluster.partition} \
---output={cluster.output} \
---error={cluster.error} \
-{cluster.extra}"
-
-
-snakemake -s /home/mcgaugheyd/git/massive_integrated_eye_scRNA/SnakePOP \
+snakemake -s $snakefile \
 -pr --jobs 1999 \
---configfile /home/mcgaugheyd/git/massive_integrated_eye_scRNA/config.yaml \
+--configfile $config \
 --use-conda \
---cluster-config /home/mcgaugheyd/git/massive_integrated_eye_scRNA/cluster.json \
---cluster "$sbcmd"  --latency-wait 120 --rerun-incomplete \
+--cluster "python3 ${cluster_config} ${cluster_json}"  --latency-wait 120 --rerun-incomplete \
 -k --restart-times 0 \
 --resources parallel=4
 
-
-conda deactivate
