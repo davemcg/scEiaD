0db0ae553c442654d3fa70309c5f322a727c0583
David McGaughey <mcgaughey@gmail.com>
2020-09-25 17:02:47 -0400

    fix output write

--- src/kallisto_run_info_aggregation.R
+++ src/kallisto_run_info_aggregation.R
@@ -10,4 +10,4 @@ for (i in f2){run_info[[i]] <- jsonlite::fromJSON(i) %>% as_tibble()}
 
 run_info <- run_info %>% bind_rows(.id = 'file') %>% mutate(sample_accession = str_extract(file, '(SRS|iPSC_RPE_scRNA_)\\d+'))
 
-write_tsv(run_info, file = 'quant/aggregated_run_info.tsv.gz')
+write_tsv(run_info, path = 'quant/aggregated_run_info.tsv.gz')
