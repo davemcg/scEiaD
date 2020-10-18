ff8a1f82449a7b4c197312d3d128504ab9f04928
David McGaughey <mcgaughey@gmail.com>
2020-10-18 09:12:59 -0400

    takes args instead of hard coded

--- src/build_QC_stats.R
+++ src/build_QC_stats.R
@@ -1,15 +1,18 @@
 library(Matrix)
 library(Seurat)
 library(tidyverse)
+args = commandArgs(trailingOnly=TRUE)
 
-load('umap/Mus_musculus_Macaca_fascicularis_Homo_sapiens__n_features5000__counts__TabulaDroplet__batch__scVI__dims8__preFilter__mindist0.1__nneighbors15.umapFilter.Rdata')
+load(args[1])
+#load('umap/Mus_musculus_Macaca_fascicularis_Homo_sapiens__n_features5000__counts__TabulaDroplet__batch__scVI__dims8__preFilter__mindist0.1__nneighbors15.umapFilter.Rdata')
 
-load('/data/mcgaugheyd/datashare/scEiaD/2020_08_13/counts_unfiltered.Rdata')
+load(args[2])
+#load('/data/mcgaugheyd/datashare/scEiaD/2020_08_13/counts_unfiltered.Rdata')
 
 sobj <- CreateSeuratObject(raw_counts)
 
 sobj[["percent.mt"]] <- PercentageFeatureSet(sobj, pattern = "^MT-")
-
+load('pipeline_data/cell_info/cell_info_labelled.Rdata')
 meta <- sobj@meta.data %>% as_tibble(rownames = 'value') %>% left_join(cell_info_labels)
 
 QC <- meta %>% mutate(QC = 
