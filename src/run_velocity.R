06e0e0ecffbc2e96186ee604be37619a0f8daea3
Vinay Siddhartha Swamy <swamyvinny@gmail.com>
2020-12-03 14:10:26 -0500

    passing back entire scvelo obj since bug with zelkonverter is fixed, so have all info to make phase portaits and other plots

--- src/run_velocity.R
+++ src/run_velocity.R
@@ -9,7 +9,9 @@ library(SingleCellExperiment)
 library(zellkonverter)
 library(reticulate)
 seurat_obj_file <- 'seurat_obj/integrated/n_features-1000__transform-counts__partition-Homo_sapiens__covariate-batch__method-scVI__dims-10__dist-0.1__neighbors-500__umap.seuratV3.Rdata'
+
 unspliced_matrix_file <- 'pipeline_data/old_clean_quant/Homo_sapiens/full_sparse_unspliced_matrix.Rdata'
+
 load_rdata <- function(x){
   load(x)
   env <- ls.str()
@@ -20,6 +22,9 @@ load_rdata <- function(x){
 
 int_seu <- load_rdata(seurat_obj_file)
 unspliced_counts <- load_rdata(unspliced_matrix_file)
+cluster_assignment <- load_rdata('pipeline_data/cluster/n_features-1000__transform-counts__partition-Homo_sapiens__covariate-batch__method-scVI__dims-10__knn-5__cluster.Rdata') %>% 
+  dplyr::rename(barcode = Barcode, cluster=cluster_knn5) %>%
+  mutate(cluster = paste0('clstr_', cluster))
 ## extract counts matrix, and 
 spliced_counts <- GetAssayData(int_seu,  slot='counts', assay = 'RNA')
 common_cells <- intersect(colnames(spliced_counts), colnames(unspliced_counts))
@@ -37,12 +42,18 @@ sce <- SingleCellExperiment(list(spliced = spliced_counts[common_genes, common_c
 rd <- lapply(int_seu@reductions, function(x) x@cell.embeddings[common_cells,])
 names(rd) <- paste0('X_', names(rd))
 reducedDims(sce) <- rd
-metadata(sce) <- int_seu@meta.data[common_cells,]
+metadata <- int_seu@meta.data[common_cells,] %>% rownames_to_column('barcode') %>%  inner_join(cluster_assignment)
+for (col in colnames(metadata)){
+  sce[[col]] <- metadata[[col]]
+  
+}
 adata <- SCE2AnnData(sce)
 use_python(glue('{conda_dir}/bin/python'))
 source_python( glue('{git_dir}/src/scvelo_wrapper.py'))
-velodf = run_velocity(adata=adata,embedding_key = 'X_scVI', dimred_key='scviUMAP')
-save(velodf, file = 'testing/homosapiens_vs_scvelo.Rdata')
+
+velo_adata = run_velocity(adata=adata,embedding_key = 'scVI', dkey='scviUMAP', vrank_gb='cluster')
+velo_sce= AnnData2SCE(velo_adata)
+save(velo_sce, file = 'testing/homosapiens_vs_scvelo_full_sce.Rdata')
 
 
 
--- src/scvelo_wrapper.py
+++ src/scvelo_wrapper.py
@@ -1,16 +1,22 @@
 import scvelo as scv
 import scanpy as sc
 import pandas as pd
-def run_velocity(adata,embedding_key, dimred_key):
+def run_velocity(adata,embedding_key, dkey, vrank_gb):
     adata.layers['spliced'] = adata.X.copy()
     scv.pp.filter_and_normalize(adata, min_shared_counts=20, n_top_genes=1000)
-    scv.pp.moments(adata, use_rep=embedding_key, n_pcs=30, n_neighbors=30)
+    ekey=f'X_{embedding_key}'
+    scv.pp.moments(adata, use_rep=ekey, n_pcs=30, n_neighbors=30)
     scv.tl.velocity(adata)
     scv.tl.velocity_graph(adata)
     scv.tl.velocity_pseudotime(adata)
     scv.tl.velocity_confidence(adata)
-    scv.tl.velocity_embedding(adata, basis=dimred_key, autoscale=False)
-    vkey=f'velocity_{dimred_key}'
-    vdf = pd.DataFrame(adata.obsm[vkey]).assign(pt=list(adata.obs['velocity_pseudotime']))
-    return vdf
+    scv.tl.velocity_embedding(adata, basis=dkey, autoscale=False)
+    scv.tl.rank_velocity_genes(adata, groupby=vrank_gb, min_corr=.3)
+    n_uns = { 'rank_velocity_genes_names' : pd.DataFrame(adata.uns['rank_velocity_genes']['names']), 
+          'rank_velocty_genes_scores' : pd.DataFrame(adata.uns['rank_velocity_genes']['scores'])
+        }
+    adata_for_transport = adata.copy()
+    del adata_for_transport.obsp
+    adata_for_transport.uns = n_uns
+    return adata_for_transport
 
