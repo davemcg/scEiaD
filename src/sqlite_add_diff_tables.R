51f3896dc6b947a152840524c8140d98d7967d28
David McGaughey <mcgaughey@gmail.com>
2020-12-04 13:05:42 -0500

    add haystack, update gene to hgnc

--- src/sqlite_add_diff_tables.R
+++ src/sqlite_add_diff_tables.R
@@ -11,6 +11,12 @@ load(args[2])
 load(args[3])
 
 PB_results <- bind_rows(PB_resultsC2, PB_resultsABC) %>% arrange(FDR, -abs(logFC))
+# update gene name
+gene_id_converter <- read_tsv('references/ensembl_biomart_human2mouse_macaque.tsv', skip = 1,
+                              col_names= c('hs_gene_id','hs_gene_id_v', 'mm_gene_id', 'mf_gene_id',
+                                           'hs_gene_name', 'mf_gene_name', 'mm_gene_name')) %>%
+  select(-hs_gene_id_v)
+PB_results <- PB_results %>% left_join(gene_id_converter %>% select(hs_gene_id, hs_gene_name) %>% unique(), by = c('Gene' = 'hs_gene_id')) %>% mutate(Gene = paste0(hs_gene_name, ' (', Gene, ')'))  %>% select(-hs_gene_name)
 
 dbWriteTable(scEiaD, 'PB_results', PB_results, overwrite = TRUE)
 db_create_index(scEiaD, table = 'PB_results', columns = c('Gene'))
@@ -89,9 +95,15 @@ cluster_labels <-
               filter(TechType == 'Well') %>%
               group_by(cluster) %>% summarise(UMAP_1 = mean(UMAP_1), UMAP_2 = mean(UMAP_2), TechType = unique(TechType)))
 
-dbWriteTable(scEiaD, 'metadata_filter', meta_filter, overwrite = TRUE)
+# haystack
+load(args[5])
+haystack <- scH$results %>% as_tibble(rownames = 'hs_gene_id') %>% left_join(gene_id_converter %>% select(hs_gene_id, hs_gene_name) %>% unique()) %>% mutate(Gene = paste0(hs_gene_name, ' (', hs_gene_id, ')')) %>% select(Gene, D_KL, `log.p.vals`, `log.p.adj`, `T.counts`)
 
+
+dbWriteTable(scEiaD, 'metadata_filter', meta_filter, overwrite = TRUE)
 dbWriteTable(scEiaD, 'celltype_predict_labels', celltype_predict_labels, overwrite = TRUE)
 dbWriteTable(scEiaD, 'celltype_labels', celltype_labels, overwrite = TRUE)
 dbWriteTable(scEiaD, 'tabulamuris_predict_labels', tabulamuris_predict_labels, overwrite = TRUE)
 dbWriteTable(scEiaD, 'cluster_labels', cluster_labels, overwrite = TRUE)
+dbWriteTable(scEiaD, 'haystack', haystack, overwrite = TRUE)
+db_create_index(scEiaD, table = 'haystack', columns = c('Gene'))
