02d4530cd62020784987d3b7edd37d0ad86e7b92
Vinay Siddhartha Swamy <swamyvinny@gmail.com>
2020-12-16 11:21:12 -0500

    fixed to use new macaque gtf

--- src/make_mito_genelists.R
+++ src/make_mito_genelists.R
@@ -4,13 +4,12 @@ save(args, file='testing/mg_args.Rdata')
 mouse_gtf <- rtracklayer::readGFF(args[1])
 maca_gtf <- rtracklayer::readGFF(args[2])
 human_gtf <- rtracklayer::readGFF(args[3])
-
-mouse_outfile <- args[5]
-maca_outfile <- args[6]
-human_outfile <- args[7]
+mouse_outfile <- args[4]
+maca_outfile <- args[5]
+human_outfile <- args[6]
 human_mito_genes <- human_gtf %>% filter(type == 'gene', grepl('^MT-', gene_name)) %>% pull(gene_id) %>% paste0('.')
 mouse_mito_genes <- mouse_gtf %>% filter(type == 'gene', grepl('^mt-', gene_name)) %>% pull(gene_id) %>% paste0('.')
-maca_mito_genes <- maca_gtf %>% filter(type == 'transcript', seqid == 'MT', !is.na(transcript_name)) %>% %>% pull(gene_id) %>% unique
+maca_mito_genes <- maca_gtf %>% filter(type == 'transcript', seqid == 'MT', !is.na(transcript_name)) %>%  pull(gene_id) %>% unique
 #weirdly, the human and mouse names get '.' added, but maca does not fo
 
 write(human_mito_genes, file = human_outfile, sep = '\n')
