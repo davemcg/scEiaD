b1173c3129aca97d063aef53a11b98e675f8c0e3
David McGaughey <mcgaughey@gmail.com>
2021-01-08 17:25:57 -0500

    add diff testing

--- src/trajectory_sling.R
+++ src/trajectory_sling.R
@@ -206,4 +206,43 @@ if (length(args) == 8) {
 	sling <- run_sling(obj_cut$seurat, obj_cut$umap$seurat_cluster_CT, reduction, ncell = nrow(obj_cut$umap))
 }
 umap_cut <- obj_cut$umap
-save(umap, umap_cut, sling, file = out)
+
+
+
+#### diff testing time
+library(tidyverse)
+library(scran)
+library(TSCAN)
+library(slingshot)
+library(tictoc)
+args = commandArgs(trailingOnly=TRUE)
+#load(args[1]) # slingshot obj
+
+pt_tester <- function(sce, pseudoTime, block){
+  pT <- colData(sce)[,pseudoTime]
+  block <- colData(sce)[,block]
+  # remove NA pseudotimes
+  pT_clean <- pT[!is.na(pT)]
+  block_clean <- block[!is.na(pT)]
+  # aggregate low n (<100) blocks into 1
+  low_n_blocks <- block_clean %>% table() %>% enframe() %>% filter(value < 200) %>% pull(name)
+  new_name <- paste(low_n_blocks, collapse = '__')
+  block_clean[block_clean %in% low_n_blocks] <- new_name
+  # diff test
+  diff_PT <- TSCAN::testPseudotime(sce[,!is.na(pT)],
+                                   pseudotime = pT_clean,
+                                   block = block_clean,
+                                   BPPARAM = BiocParallel::MulticoreParam(12))
+  diff_PT
+}
+
+sce <- sling$sling
+diffPT <- list()
+for (i in colnames(colData(sce)) %>% grep('slingP', .,  value = TRUE)){
+  print(i)
+  tic()
+  diffPT[[i]] <- try({pt_tester(sce, i, 'study_accession')})
+  toc()
+}
+
+save(umap, umap_cut, sling, diffPT, file = out
