4af39e2a02d091365421a09e67dd2bfd5872e643
David McGaughey <mcgaughey@gmail.com>
2021-03-20 10:42:09 -0400

    aggregative kallisto run_injo

--- src/kallisto_run_info_aggregation.R
+++ src/kallisto_run_info_aggregation.R
@@ -1,13 +1,11 @@
 library(tidyverse)
 
-f <- list.files('quant/', recursive = TRUE, pattern = 'run_info.json', full.names=TRUE)
-
-f2 <- grep('old', f, value = TRUE, invert = TRUE)
+dirs <- list.files('/data/OGVFB_BG/scEiaD/2021_01_21/', recursive=TRUE, pattern = 'run_info.json', full.names = TRUE)
 
 run_info <- list()
 
-for (i in f2){run_info[[i]] <- jsonlite::fromJSON(i) %>% as_tibble()}
+for (i in dirs){run_info[[i]] <- jsonlite::fromJSON(file = i) %>% as_tibble()}
 
 run_info <- run_info %>% bind_rows(.id = 'file') %>% mutate(sample_accession = str_extract(file, '(SRX|EGAF|ERS|SRS|iPSC_RPE_scRNA_)\\d+'))
 
-write_tsv(run_info, path = 'quant/aggregated_run_info.tsv.gz')
+write_tsv(run_info, file  = 'aggregated_run_info.tsv.gz')
