2f79145e67898bd057996480020b1ab64ddfedc8
David McGaughey <mcgaughey@gmail.com>
2021-05-27 21:20:11 -0400

    prototype to grab biosample info

---
+++ src/biosample_attribute_grabber.R
@@ -0,0 +1,28 @@
+library(XML)
+library(reutils)
+library(tidyverse)
+
+eutil_grab <- efetch(uid = 'SAMN16523262', db = 'biosample', retmode = 'xml') 
+xml_list_obj <- eutil_grab[["//Attributes"]] %>% XML::xmlToList()
+
+attribute_finder <- function(xml_list_obj){
+  out <- list()
+  for (i in 1:length(xml_list_obj)){
+    out[[i]] <- xml_list_obj[[i]]$.attrs['display_name'] 
+  }
+  return(out)
+}
+
+value_grabber <- function(attribute, xml_list_obj){
+  for (i in 1:length(xml_list_obj)){
+    if (xml_list_obj[[i]]$.attrs['display_name'] == attribute){
+      out <- xml_list_obj[[i]]$text
+      return(out)
+    }
+  }
+}
+
+attribute_df <-  attribute_finder(xml_list_obj) %>% as.character() %>% data.frame()
+colnames(attribute_df)[1] <- 'attribute'
+attribute_df <- attribute_df %>% rowwise() %>% mutate(value = value_grabber(attribute, xml_list_obj))
+attribute_df$id = args[1]
