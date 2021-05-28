8234e90d6bae8f9419a713baf4fc3836500fe254
David McGaughey <mcgaughey@gmail.com>
2021-05-28 11:16:03 -0400

    typo fix

--- analysis/2021_05__newData.R
+++ analysis/2021_05__newData.R
@@ -3,7 +3,7 @@ library(tidyverse)
 srp292721 <- read_tsv('data/SRP292721_meta.tsv.gz')
 srp212788 <- read_tsv('data/SRP212788_meta.tsv.gz')
 srp254408 <- read_tsv('data/SRP254408_meta.tsv.gz')
-srp255874 <- read_tsv('data/SRP255871_meta.tsv.gz')
+srp255874 <- read_tsv('data/SRP255874_meta.tsv.gz')
 srp255871 <- read_tsv('data/SRP255871_meta.tsv.gz')
 srp251245 <- read_tsv('data/SRP251245_meta.tsv.gz')
 srp238072 <- read_tsv('data/SRP238072_meta.tsv.gz')
@@ -36,7 +36,7 @@ new_meta <- raw_meta %>%
          bam10x = str_extract(bam_url, '\\w+\\.bam'),
          Comment = NA,
          TissueNote = paste0(`EXPERIMENT.TITLE`, ' | ', `SAMPLE.SAMPLE_ATTRIBUTES.SAMPLE_ATTRIBUTE.VALUE`, ' | ', `SAMPLE.TITLE`)) %>% 
-  select(sample_accession = `EXPERIMENT.STUDY_REF.IDENTIFIERS.PRIMARY_ID`,
+  select(sample_accession = `EXPERIMENT.DESIGN.SAMPLE_DESCRIPTOR.IDENTIFIERS.PRIMARY_ID`,
          run_accession = `RUN_SET.RUN.IDENTIFIERS.PRIMARY_ID`,
          library_layout = LibraryLayout,
          organism = ScientificName,
@@ -81,10 +81,13 @@ new_meta <- raw_meta %>%
                               study_accession == 'SRP255871' ~ '10xv2',
                               study_accession == 'SRP238072' ~ '10xv2',
                               study_accession == 'SRP286543' ~ '10xv2',
+                              study_accession == 'SRP255874' ~ '10xv2',
                               study_accession == 'SRP310237' ~ '10xv2',
                               study_accession == 'SRP251245' ~ '10xv3'),
          Tissue = case_when(study_accession == 'SRP251245' ~ 'Outflow Tract',
                             study_accession == 'SRP254408' ~ 'Outflow Tract',
+                            study_accession == 'SRP255871' ~ 'Outflow Tract',
+                            study_accession == 'SRP255874' ~ 'Outflow Tract',
                             study_accession == 'SRP238072' ~ 'Retina',
                             study_accession == 'SRP310237' ~ 'Brain Choroid Plexus',
                             study_accession == 'SRP238072' ~ 'Retina',
@@ -94,7 +97,23 @@ new_meta <- raw_meta %>%
                             study_accession == 'SRP212788' ~ 'Retina'),
          UMI = case_when(study_accession == 'SRP212788' ~ 'No',
                          TRUE ~ 'Yes'),
-         Age = case_when(study_accession == 'SRP292721' ~ 'Adult'
+         Age = case_when(study_accession == 'SRP292721' ~ 'Adult',
+                         study_accession == 'SRP212788' ~ 'Adult',
+                         sample_accession == 'SRS6393469' ~ as.character(365 * 73),
+                         sample_accession == 'SRS6393471' ~ as.character(365 * 83),
+                         sample_accession == 'SRS6393470' ~ as.character(365 * 56),
+                         sample_accession == 'SRS6393468' ~ as.character(365 * 55),
+                         biosample == 'SAMN14567342' ~ as.character(365 * 65),
+                         biosample == 'SAMN14567343' ~ as.character(365 * 56),
+                         biosample == 'SAMN14567344' ~ as.character(365 * 64),
+                         biosample == 'SAMN14567345' ~ as.character(365 * 60),
+                         biosample == 'SAMN14567346' ~ as.character(365 * 78),
+                         biosample == 'SAMN14567347' ~ as.character(365 * 78),
+                         biosample == 'SAMN14567348' ~ as.character(365 * 74),
+                         biosample == 'SAMN14567337' ~ as.character(365 * 10),
+                         biosample == 'SAMN14567338' ~ as.character(365 * 10),
+                         
+                         
          )
   )
 
