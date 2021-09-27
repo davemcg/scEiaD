69c5a06b13cdfd309dbc73a4e48f9ea10aff21fb
David McGaughey <mcgaughey@gmail.com>
2021-09-26 23:01:17 -0400

    remove unused, add some new categories, HVG hangling

--- src/build_seurat_obj_classic.R
+++ src/build_seurat_obj_classic.R
@@ -42,7 +42,13 @@ cell_info <- cell_info %>%
   mutate(batch = case_when(study_accession == 'SRP125998' ~ paste0(study_accession, "_", Platform, '_NA'),
                            TRUE ~ batch)) %>% 
   mutate(batch = gsub(' ', '_', batch))
-
+HVG_var_names =  scan(rule$input$HVG_var_names, what = 'character')
+# cut down to approx max HVG length 
+# assuming the list is in order of most var to least var
+HVG_var_names = HVG_var_names[1:n_features] 
+if (sum(is.na(HVG_var_names)) > 0) {
+	HVG_var_names = HVG_var_names[!is.na(HVG_var_names)]
+}
 mito_geneids = scan(rule$input$mitogene_list, character(), sep= '\n') %>% 
   str_remove_all('\\.\\d+\\.$')
 load_rdata <- function(x){
@@ -53,7 +59,9 @@ load_rdata <- function(x){
   return(get(var))
 }
 
-m <- load_rdata(rule$input$all_species_quant_file)
+m <- load_rdata(rule$input$quant_file)
+
+
 
 print('Splitting time')
 # # custom combos / sets
@@ -62,100 +70,20 @@ source(glue('{git_dir}/src/make_seurat_obj_functions.R') )
 
 # load study exlucions list
 exclude <- scan(glue('{git_dir}/data/exclusion.txt'), what = 'character') 
-cell_info <- cell_info %>% filter(!study_accession %in% exclude)
+cell_info <- cell_info %>% filter(!study_accession %in% exclude, value %in% colnames(m))
 m <- m[, cell_info %>% pull(value)]
 
-if (set == 'early'){
-  print("Running Early")
-  m_early <- m[,cell_info %>% filter(Age < 10) %>% pull(value)]
-  seurat__standard <- make_seurat_obj(m_early,cell_info, split.by = covariate,mito_geneids=mito_geneids)
-} else if (set == 'late'){
-  print("Running Late")
-  m_late <- m[,cell_info %>% filter(Age >= 10) %>% pull(value)]
-  seurat__standard <- make_seurat_obj(m_late,cell_info, split.by = covariate,mito_geneids=mito_geneids)
-} else if (set == 'full'){
-  print("Running Full")
-  seurat__standard <- make_seurat_obj(m,cell_info, split.by = covariate,mito_geneids=mito_geneids)
-} else if (set == 'onlyDROPLET'){
-  print("Running onlyDROPLET (remove well based)")
-  m_onlyDROPLET <- m[,cell_info %>% filter(Platform %in% c('DropSeq', '10xv2', '10xv3'), study_accession != 'SRP131661') %>% pull(value)]
-  seurat__standard <- make_seurat_obj(m_onlyDROPLET, cell_info, split.by = covariate, keep_well = FALSE, mito_geneids = mito_geneids)
-}  else if (set == 'TabulaDroplet'){
-  print("Running onlyDROPLET with Tabula Muris (no well)")
-  #m_TABULA_DROPLET <- m[,cell_info %>% filter(Platform %in% c('DropSeq', '10xv2', '10xv3')) %>% pull(value)]
-  m_TABULA_DROPLET <- m[,cell_info %>% filter(value %in% colnames(m), Platform %in% c('DropSeq', '10xv2', '10xv3'), !Source %in% c('Organoid', 'Cell Culture')) %>% pull(value)]
-  seurat__standard <- make_seurat_obj(m_TABULA_DROPLET,cell_info, split.by = covariate, keep_well = FALSE,mito_geneids=mito_geneids)
-} else if (set == 'TabulaDropletLabelled'){
-  print("Running onlyDROPLET labelled samples only with Tabula Muris (no well)")
-  cells <- load_rdata('testing/TabulaDropletLabelled_barcodes.Rdata') %>% 
-    .[.%in% colnames(m)]
-  m <- m[,cells]
-  seurat__standard <- make_seurat_obj(m,cell_info, split.by = covariate, keep_well = FALSE,mito_geneids=mito_geneids)
-}else if (set == 'onlyWELL' & transform == 'counts'){
-  print("Running onlyWELL with quminorm (remove droplet based)") 
-  m_onlyWELL <- m[,cell_info %>% filter(!Platform %in% c('DropSeq', '10xv2', '10xv3'), study_accession != 'SRP131661') %>% pull(value)]
-  seurat__standard <- make_seurat_obj(m_onlyWELL,cell_info, split.by = covariate, keep_droplet = FALSE, qumi = TRUE,mito_geneids=mito_geneids)
-} else if (set == 'onlyWELL') {
-  print("Running onlyWELL (remove droplet based)") 
-  m_onlyWELL <- m[,cell_info %>% filter(!Platform %in% c('DropSeq', '10xv2', '10xv3'), study_accession != 'SRP131661') %>% pull(value)]
-  seurat__standard <- make_seurat_obj(m_onlyWELL,cell_info, split.by = covariate, keep_droplet = FALSE, mito_geneids=mito_geneids)
-} else if (set == 'downsample'){
-  print("Running downsample")
-  downsample_samples <- 
-    cell_info %>% 
-    group_by(batch) %>% 
-    sample_n(2000, replace = TRUE) %>% 
-    unique() %>% 
-    pull(value)
-  m_downsample <- m[,downsample_samples]
-  seurat__standard <- make_seurat_obj(m_downsample,cell_info, split.by = covariate,mito_geneids=mito_geneids)
-} else if (set == 'universe'){
+if (set == 'universe'){
   seurat__standard <- make_seurat_obj(m[, cell_info %>% filter(!Source %in% c('Organoid', 'Cell Culture')) %>% pull(value)], cell_info, split.by = covariate, lengthCor = TRUE, dont_use_well_for_FVF = TRUE, mito_geneids = mito_geneids)
-} else if (set == 'universeX'){
-  seurat__standard <- make_seurat_obj(m, cell_info, split.by = covariate, lengthCor = TRUE, only_use_human_for_FVF = TRUE, mito_geneids = mito_geneids)
-} else if (set %in% c('cones', 'hc', 'rgc', 'amacrine', 'mullerglia', 'bipolar', 'rods' )){
-  # no circular dependencies!
-  precursors <- c('AC/HC_Precurs', 'Early RPCs', 'Late RPCs', 'Neurogenic Cells', 'Photoreceptor Precursors', 'RPCs')
-  if (set == 'cones') {
-  	m_subset = m[, cell_info %>% filter(CellType %in% c('Cones', precursors)) %>% pull(value)]
-  }
-  if (set == 'bipolar') {
-  	m_subset = m[, cell_info %>% filter(CellType %in% c('Bipolar Cells', precursors)) %>% pull(value)]
-  }
-  if (set == 'rods') {
-  	m_subset = m[, cell_info %>% filter(CellType %in% c('Rods', precursors)) %>% pull(value)]
-  }
-  if (set == 'mullerglia') {
-  	m_subset = m[, cell_info %>% filter(CellType %in% c('Muller Glia', precursors)) %>% pull(value)]
-  }
-  if (set == 'amacrine') {
-  	m_subset = m[, cell_info %>% filter(CellType %in% c('Amacrine Cells', precursors)) %>% pull(value)]
-  }
-  if (set == 'rgc') {
-  	m_subset = m[, cell_info %>% filter(CellType %in% c('Retinal Ganglion Cells', precursors)) %>% pull(value)]
-  }
-  if (set == 'hc') {
-  	m_subset = m[, cell_info %>% filter(CellType %in% c('Horizontal Cells', precursors)) %>% pull(value)]
-  }
-  seurat__standard <- make_seurat_obj(m_subset, cell_info, split.by = covariate, keep_well = FALSE, mito_geneids = mito_geneids)
-} else if (set == 'raw') {
-  seurat__standard <- m
-} else if(set %in% c('Homo_sapiens', 'Mus_musculus')){ ##hacky way to loading in species specific data
-  print(glue('loading {set} quant') )
-  ## need to keep squaring off the data to keep uniformity for seurat
-  
-  if (set == 'Mus_musculus'){
-    mito_geneids=scan('references/mito_genes/mm-mus_musculus_mitogenes.txt', character(), sep = '\n') %>% 
-      str_remove_all('\\.\\d+\\.$')
-    
-  }
-  
-  species <- set 
-  m_file <- str_replace_all(rule$input$all_species_quant_file, 'all_species_', glue('{species}/'))
-  spec_m = load_rdata(m_file)
-  seurat__standard =  make_seurat_obj(spec_m,cell_info, split.by = covariate, keep_well = FALSE, mito_geneids=mito_geneids)
+} else if (set == 'universeHUMANHVG'){
+  seurat__standard <- make_seurat_obj(m[, cell_info %>% filter(!Source %in% c('Organoid', 'Cell Culture')) %>% pull(value)], cell_info, split.by = covariate, lengthCor = TRUE, dont_use_well_for_FVF = TRUE, mito_geneids = mito_geneids, only_use_human_for_FVF = TRUE)
+} else if (set == 'universeGIGAHVG'){
+  seurat__standard <- make_seurat_obj(m[, cell_info %>% filter(!Source %in% c('Organoid', 'Cell Culture')) %>% pull(value)], cell_info, split.by = covariate, lengthCor = TRUE, dont_use_well_for_FVF = TRUE, mito_geneids = mito_geneids,  HVG = HVG_var_names)
+} else if (set %in% c('mouse')){
+  seurat__standard <- make_seurat_obj(m[, cell_info %>% filter(!Source %in% c('Organoid', 'Cell Culture')) %>% pull(value)], cell_info, split.by = covariate, lengthCor = TRUE, dont_use_well_for_FVF = TRUE, mito_geneids = mito_geneids)
+} else if (set %in% c('chick', 'macaque', 'human')){
+  seurat__standard <- make_seurat_obj(m[, cell_info %>% filter(!Source %in% c('Organoid', 'Cell Culture')) %>% pull(value)], cell_info, split.by = covariate, lengthCor = TRUE, dont_use_well_for_FVF = TRUE, mito_geneids = mito_geneids, keep_well = FALSE)
 } 
-
 
 
 if (transform == 'SCT'){
