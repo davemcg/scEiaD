8584d9cc09ccae91c5fcf71e15fef7a520b61e45
David McGaughey <mcgaughey@gmail.com>
2021-11-13 08:35:08 -0500

    test scripts, update meta

--- analysis/asess_integration.R
+++ analysis/asess_integration.R
@@ -1,6 +1,6 @@
 # celltype prediction accuracy
-load('~/data/scEiaD_v2/merged_xgboost_2021-09-20.Rdata')
-load('~/data/scEiaD_v2/merged_stats_2021-09-20.Rdata')
+load('~/data/scEiaD_v3/merged_xgboost_2021-11-05.Rdata')
+load('~/data/scEiaD_v3/merged_stats_2021-11-05.Rdata')
 library(tidyverse)
 library(cowplot)
 accuracy <- accuracy %>% 
@@ -24,47 +24,53 @@ accuracy <- accuracy %>%
 #   select(Score, Group, Value, set, dims:normalization) %>% 
 #   filter(Score %in% c('LISI','Silhouette')) 
 
-lisi <- perf %>% 
-  filter(Score == 'LISI') %>%
-  pivot_wider(values_from = Value, names_from = c('Group')) %>% 
-  ggplot(aes(y=Batch, x=-Cluster)) + geom_point(aes(color=as.factor(dims), shape = as.factor(nf)), size = 5) +
-  cowplot::theme_cowplot() + scale_color_manual(values = pals::alphabet() %>% unname()) +
-  ggtitle('LISI') 
+# lisi <- perf %>% 
+#   filter(Score == 'LISI') %>%
+#   pivot_wider(values_from = Value, names_from = c('Group')) %>% 
+#   ggplot(aes(y=Batch, x=-Cluster)) + geom_point(aes(color=as.factor(dims), shape = as.factor(nf)), size = 5) +
+#   cowplot::theme_cowplot() + scale_color_manual(values = pals::alphabet() %>% unname()) +
+#   ggtitle('LISI') 
+# 
+# ari <- perf %>% 
+#   filter(Score == 'ARI', Group == 'CellType-Cluster') %>%
+#   ggplot(aes(y=Value, x=normalization)) + geom_jitter(aes(color=as.factor(dims), shape = as.factor(nf)), size = 5, width = 0.2) +
+#   cowplot::theme_cowplot() + scale_color_manual(values = pals::alphabet() %>% unname()) + ylab('ARI') +
+#   ggtitle('ARI')
+# 
+# nmi <- perf %>% 
+#   filter(Score == 'NMI', Group == 'CellType-Cluster', nf == 2000, dims %in% c(8,30), knn == 20) %>%
+#   ggplot(aes(y=Value, x=normalization)) + geom_jitter(aes(color=method, shape = normalization, alpha = dims), size = 5, width = 0.2) +
+#   cowplot::theme_cowplot() + scale_color_manual(values = pals::alphabet() %>% unname()) + ylab('NMI') +
+#   ggtitle('NMI') + theme(legend.position="none") +
+#   scale_alpha_continuous(range = c(0.6, 1), breaks =c(8,30))
+# 
+# silhouette <- perf %>% filter(Score == 'Silhouette') %>% 
+#   pivot_wider(values_from = Value, names_from = c('Group')) %>% 
+#   ggplot(aes(y=-Batch, x=Cluster, shape = as.factor(nf), color = as.factor(dims))) + geom_point(size = 5) +
+#   cowplot::theme_cowplot() + scale_color_manual(values = pals::alphabet() %>% unname()) +
+#   ggtitle("Silhouette")
+# 
+# legend <- get_legend(
+#   # create some space to the left of the legend
+#   lisi + theme(legend.box.margin = margin(0, 0, 0, 12))
+# )
+accuracy %>% 
+  filter(partition %in% c('universe')) %>% 
+  group_by(norm, nf, dims, method, knn, epochs) %>% 
+  summarise(bad_score_sum = sum(score < 0.8), xgboost_score = mean(score)) %>% 
+  arrange(-xgboost_score)
 
-ari <- perf %>% 
-  filter(Score == 'ARI', Group == 'CellType-Cluster') %>%
-  ggplot(aes(y=Value, x=normalization)) + geom_jitter(aes(color=as.factor(dims), shape = as.factor(nf)), size = 5, width = 0.2) +
-  cowplot::theme_cowplot() + scale_color_manual(values = pals::alphabet() %>% unname()) + ylab('ARI') +
-  ggtitle('ARI')
-
-nmi <- perf %>% 
-  filter(Score == 'NMI', Group == 'CellType-Cluster', nf == 2000, dims %in% c(8,30), knn == 20) %>%
-  ggplot(aes(y=Value, x=normalization)) + geom_jitter(aes(color=method, shape = normalization, alpha = dims), size = 5, width = 0.2) +
-  cowplot::theme_cowplot() + scale_color_manual(values = pals::alphabet() %>% unname()) + ylab('NMI') +
-  ggtitle('NMI') + theme(legend.position="none") +
-  scale_alpha_continuous(range = c(0.6, 1), breaks =c(8,30))
-
-silhouette <- perf %>% filter(Score == 'Silhouette') %>% 
-  pivot_wider(values_from = Value, names_from = c('Group')) %>% 
-  ggplot(aes(y=-Batch, x=Cluster, shape = as.factor(nf), color = as.factor(dims))) + geom_point(size = 5) +
-  cowplot::theme_cowplot() + scale_color_manual(values = pals::alphabet() %>% unname()) +
-  ggtitle("Silhouette")
-
-legend <- get_legend(
-  # create some space to the left of the legend
-  lisi + theme(legend.box.margin = margin(0, 0, 0, 12))
-)
-
-purity <- 1; balance <- 3; mixing <- 3
+purity <- 1; balance <- 3; mixing <- 1
 partition = 'universe'
 method = 'scvIprojection'
-perf %>%  
-  filter(partition %in% c('universe','universeHVG')) %>% 
+perf %>% 
+  filter(partition %in% c('universe')) %>% 
   select(-clusterN, -clusterMedian, -subset) %>% 
   pivot_wider(names_from = c('Score','Group'), values_from = Value) %>% 
   left_join(accuracy %>% 
-              filter(partition %in% c('universe','universeHVG')) %>% 
-              group_by(norm, nf, dims, method, knn, epochs) %>% summarise(xgboost_score = mean(score))) %>% 
+              filter(partition %in% c('universe')) %>% 
+              group_by(norm, nf, dims, method, knn, epochs) %>% 
+              summarise(bad_score_sum = sum(score < 0.8), xgboost_score = mean(score))) %>% 
   mutate(sumZScale =
            -scale(LISI_CellType)[,1] * purity +
            scale(Silhouette_CellType)[,1] * purity  +
@@ -76,16 +82,16 @@ perf %>%
            scale(`NMI_CellType-Cluster`)[,1] * balance  +
            scale(`ARI_CellType-Cluster`)[,1]* balance  +
            
-           scale(LISI_Batch)[,1] * mixing  + # Z score
+           scale(LISI_Batch)[,1] * mixing  %>%   # Z score
            -scale(Silhouette_Batch)[,1] * mixing) %>% 
   mutate(nf = as.factor(nf),
          `scVI latent dims` = as.factor(dims),
          method = gsub('scVIprojection', 'scVI-projection',method),
          method = gsub('scVI$', 'scVI-standard', method),
          method = factor(method, levels = c('scVI-standard','scVI-projection'))) %>% 
-  ggplot(aes(x=sumZScale, y = nf, color = `scVI latent dims`, shape = method)) + 
-  #ggbeeswarm::geom_quasirandom(groupOnX=FALSE) +
-  geom_point() +
+  ggplot(aes(x=sumZScale, y = nf, color = `scVI latent dims`, shape = as.factor(epochs))) + 
+  ggbeeswarm::geom_quasirandom(groupOnX=FALSE, size = 5) +
+  #geom_point() +
   facet_wrap(~set) +
   scale_color_manual(values = pals::brewer.set1(n = 10) %>% unname()) +
   scale_fill_manual(values = c('white','gray')) +
--- analysis/xgboost_umap_view_play.R
+++ analysis/xgboost_umap_view_play.R
@@ -2,14 +2,21 @@ library(tidyverse)
 library(cowplot)
 library(scattermore)
 library(glue)
-load('~/data/scEiaD_v2/xgboost_predictions/n_features-5000__transform-counts__partition-universe__covariate-batch__method-scVIprojection__dims-10__epochs-5__dist-0.1__neighbors-50__knn-20__umapPredictions.Rdata')
+load('~/data/scEiaD_v3/xgboost_predictions13/n_features-6000__transform-counts__partition-universe__covariate-batch__method-scVIprojection__dims-10__epochs-15__dist-0.1__neighbors-50__knn-20__umapPredictions.Rdata')
+#load('~/data/scEiaD_v2/org_umap.Rdata')
 ncells <- nrow(umap)
 
-labels <- umap %>% group_by(CellType_predict) %>% 
+#umap <- umap %>% left_join(., org_umap %>% select(Barcode, UMAPx1 = scviUMAP_1, UMAPx2 = scviUMAP_2 ))
+                           
+
+labels <- umap %>% 
+  #filter(organism == 'Gallus gallus') %>% 
+  group_by(CellType_predict) %>% 
   summarise(UMAP_1 = mean(UMAP_1),
             UMAP_2 = mean(UMAP_2))
 # pan view
 umap %>% 
+  #filter(CellType_predict == 'Epithelial') %>% 
   mutate(CellType_predict = case_when(is.na(CellType_predict) ~ 'Unlabelled',
                                       TRUE ~ CellType_predict)) %>% 
   ggplot() + 
@@ -21,10 +28,76 @@ umap %>%
   ggrepel::geom_label_repel(data = labels, aes(x=UMAP_1, y=UMAP_2, label = CellType_predict ), alpha = 0.8, size = 3) +
   scale_color_manual(values = c(pals::alphabet(), pals::alphabet2(), pals::glasbey()) %>% unname()) +
   theme(axis.text.x=element_text(angle = 90, vjust = 0.5)) +
+  ggtitle(glue('Number of Cells: {ncells}'))
+
+# split by org
+umap %>% 
+  mutate(CellType_predict = case_when(is.na(CellType_predict) ~ 'Unlabelled',
+                                      TRUE ~ CellType_predict)) %>% 
+  ggplot() + 
+  geom_scattermore(aes(x=UMAP_1, 
+                       y = UMAP_2, 
+                       colour = CellType_predict), pointsize = 3, alpha = 0.1) + 
+  guides(colour = guide_legend(override.aes = list(size=8, alpha = 1))) + 
+  theme_cowplot() + 
+  #ggrepel::geom_label_repel(data = labels, aes(x=UMAP_1, y=UMAP_2, label = CellType_predict ), alpha = 0.8, size = 3) +
+  scale_color_manual(values = c(pals::alphabet(), pals::alphabet2(), pals::glasbey()) %>% unname()) +
+  theme(axis.text.x=element_text(angle = 90, vjust = 0.5)) +
+  ggtitle(glue('Number of Cells: {ncells}'))  +
+  facet_wrap(~organism)
+
+
+# split by study
+umap %>% 
+  mutate(CellType_predict = case_when(is.na(CellType_predict) ~ 'Unlabelled',
+                                      TRUE ~ CellType_predict)) %>% 
+  ggplot() + 
+  geom_scattermore(aes(x=UMAP_1, 
+                       y = UMAP_2, 
+                       colour = CellType_predict), pointsize = 5, alpha = 0.1) + 
+  guides(colour = guide_legend(override.aes = list(size=8, alpha = 1))) + 
+  theme_cowplot() + 
+  #ggrepel::geom_label_repel(data = labels, aes(x=UMAP_1, y=UMAP_2, label = CellType_predict ), alpha = 0.8, size = 3) +
+  scale_color_manual(values = c(pals::alphabet(), pals::alphabet2(), pals::glasbey()) %>% unname()) +
+  theme(axis.text.x=element_text(angle = 90, vjust = 0.5)) +
+  ggtitle(glue('Number of Cells: {ncells}'))  +
+  facet_wrap(~study_accession)
+
+# chick only
+umap %>% 
+  filter(organism == 'Gallus gallus') %>% 
+  mutate(CellType_predict = case_when(is.na(CellType_predict) ~ 'Unlabelled',
+                                      TRUE ~ CellType_predict)) %>% 
+  ggplot() + 
+  geom_scattermore(aes(x=UMAP_1, 
+                       y = UMAP_2, 
+                       colour = CellType_predict), pointsize = 3, alpha = 0.1) + 
+  guides(colour = guide_legend(override.aes = list(size=8, alpha = 1))) + 
+  theme_cowplot() + 
+  #ggrepel::geom_label_repel(data = labels, aes(x=UMAP_1, y=UMAP_2, label = CellType_predict ), alpha = 0.8, size = 3) +
+  scale_color_manual(values = c(pals::alphabet(), pals::alphabet2(), pals::glasbey()) %>% unname()) +
+  theme(axis.text.x=element_text(angle = 90, vjust = 0.5)) +
+  ggtitle(glue('Number of Cells: {ncells}'))  +
+  facet_wrap(~biosample_title)
+
+
+        ## cluster
+umap %>% 
+  ggplot() + 
+  geom_scattermore(aes(x=UMAP_1, 
+                       y = UMAP_2, 
+                       colour = as.factor(cluster)), pointsize = 0, alpha = 0.1) + 
+  guides(colour = guide_legend(override.aes = list(size=8, alpha = 1))) + 
+  theme_cowplot() + 
+  ggrepel::geom_label_repel(data = labels, aes(x=UMAP_1, y=UMAP_2, label = CellType_predict ), alpha = 0.8, size = 3) +
+  scale_color_manual(values = c(pals::alphabet(), pals::alphabet2(), pals::glasbey()) %>% unname()) +
+  theme(axis.text.x=element_text(angle = 90, vjust = 0.5)) +
   ggtitle(glue('Number of Cells: {ncells}')) 
+
 
 # facet by cell type
 umap %>% 
+  filter(study_accession != 'SRP131661') %>% 
   mutate(CellType_predict = case_when(is.na(CellType_predict) ~ 'Unlabelled',
                                       TRUE ~ CellType_predict)) %>% 
   ggplot() + 
@@ -38,17 +111,22 @@ umap %>%
   ggtitle(glue('Number of Cells: {ncells}')) + facet_wrap(~CellType_predict) 
 
 # RPE view
-umap %>% filter(CellType_predict == 'RPE') %>% ggplot() + 
+umap %>% filter(CellType == 'Schwann') %>% ggplot() + 
   geom_scattermore(aes(x=UMAP_1, 
-                       y = UMAP_2), pointsize = 5, alpha = 0.2) + 
+                       y = UMAP_2, color = study_accession), pointsize = 3, alpha = 0.2) + 
   guides(colour = guide_legend(override.aes = list(size=8, alpha = 1))) + 
   theme_cowplot() +
-  scale_color_manual(values = c(pals::alphabet2(), pals::alphabet(), pals::glasbey()) %>% unname()) +
+  scale_color_manual(values = c(pals::alphabet(), pals::alphabet(), pals::glasbey()) %>% unname()) +
   theme(axis.text.x=element_text(angle = 90, vjust = 0.5)) +
-  ggtitle(glue('Number of Cells: {ncells}')) + facet_wrap(~study_accession) 
+  ggtitle(glue('Number of Cells: {ncells}')) 
+
 
 
 # Quick Bharti RPE assess (should be mostly RPE/Fibro/Endo/Epi)
-umap %>% group_by(CellType_predict, study_accession) %>% summarise(Count = n()) %>% filter(study_accession == 'Bharti_Nguyen_iRPE_2D_3D')
+umap %>% group_by(CellType_predict, study_accession) %>% summarise(Count = n()) %>% filter(study_accession == 'Bharti_Nguyen_iRPE_2D_3D') %>% arrange(-Count)
 # Cluster <-> CellType_predict
-umap %>% group_by(cluster, CellType_predict) %>% summarise(Count = n()) %>% mutate(Percent = (Count / sum(Count)) * 100) %>% filter(Percent > 5) %>% arrange(cluster, -Percent) %>% DT::datatable()
+umap %>% 
+  mutate(cluster = as.factor(cluster), CellType_predict = as.factor(CellType_predict)) %>% 
+  group_by(cluster, CellType_predict, study_accession) %>% summarise(Count = n()) %>% mutate(Percent = (Count / sum(Count)) * 100) %>% filter(Percent > 5) %>% arrange(cluster, -Percent) %>% DT::datatable(
+                                                                                                                                                                                                  filter = list(position = 'bottom', clear = TRUE, plain = TRUE),
+                                                                                                                                                                                                  options = list(pageLength = 10,  searchHighlight = TRUE, dom = 'frtBip'))
