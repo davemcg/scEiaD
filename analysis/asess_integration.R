9330473698fc1711e6f99500e3b8b5b9faa6a0ed
David McGaughey <mcgaughey@gmail.com>
2022-03-01 11:31:30 -0500

    update integration plot

--- analysis/asess_integration.R
+++ analysis/asess_integration.R
@@ -1,6 +1,6 @@
 # celltype prediction accuracy
-load('~/data/scEiaD_v2/merged_xgboost_2021-09-20.Rdata')
-load('~/data/scEiaD_v2/merged_stats_2021-09-20.Rdata')
+load('~/data/scEiaD_2022_02/results/merged_stats.Rdata')
+load('~/data/scEiaD_2022_02/results/merged_xgboost.Rdata')
 library(tidyverse)
 library(cowplot)
 accuracy <- accuracy %>% 
@@ -14,78 +14,53 @@ accuracy <- accuracy %>%
     knn = str_extract(file, 'knn-\\d+\\.\\d+|knn-\\d+') %>% gsub('knn-', '', .) %>% as.numeric(),
     epochs = str_extract(file, 'epochs-\\d+') %>% gsub('epochs-','',.) %>% as.numeric())
 
-
-#perf <- perf %>% filter(grepl('scVI',method)) 
-#cluster_stats <- perf %>% select(clusterN, clusterMedian, dims, nf, knn, method, normalization, set) %>% unique() 
-
-#cluster_stats <- perf %>% select(clusterN, clusterMedian, dims, nf, knn, method, normalization, set) %>% unique() 
-# 
-# perf_well <- perf %>% unique() %>% filter(set == 'onlyWELL') %>% 
-#   select(Score, Group, Value, set, dims:normalization) %>% 
-#   filter(Score %in% c('LISI','Silhouette')) 
-
-lisi <- perf %>% 
-  filter(Score == 'LISI') %>%
-  pivot_wider(values_from = Value, names_from = c('Group')) %>% 
-  ggplot(aes(y=Batch, x=-Cluster)) + geom_point(aes(color=as.factor(dims), shape = as.factor(nf)), size = 5) +
-  cowplot::theme_cowplot() + scale_color_manual(values = pals::alphabet() %>% unname()) +
-  ggtitle('LISI') 
-
-ari <- perf %>% 
-  filter(Score == 'ARI', Group == 'CellType-Cluster') %>%
-  ggplot(aes(y=Value, x=normalization)) + geom_jitter(aes(color=as.factor(dims), shape = as.factor(nf)), size = 5, width = 0.2) +
-  cowplot::theme_cowplot() + scale_color_manual(values = pals::alphabet() %>% unname()) + ylab('ARI') +
-  ggtitle('ARI')
-
-nmi <- perf %>% 
-  filter(Score == 'NMI', Group == 'CellType-Cluster', nf == 2000, dims %in% c(8,30), knn == 20) %>%
-  ggplot(aes(y=Value, x=normalization)) + geom_jitter(aes(color=method, shape = normalization, alpha = dims), size = 5, width = 0.2) +
-  cowplot::theme_cowplot() + scale_color_manual(values = pals::alphabet() %>% unname()) + ylab('NMI') +
-  ggtitle('NMI') + theme(legend.position="none") +
-  scale_alpha_continuous(range = c(0.6, 1), breaks =c(8,30))
-
-silhouette <- perf %>% filter(Score == 'Silhouette') %>% 
-  pivot_wider(values_from = Value, names_from = c('Group')) %>% 
-  ggplot(aes(y=-Batch, x=Cluster, shape = as.factor(nf), color = as.factor(dims))) + geom_point(size = 5) +
-  cowplot::theme_cowplot() + scale_color_manual(values = pals::alphabet() %>% unname()) +
-  ggtitle("Silhouette")
-
-legend <- get_legend(
-  # create some space to the left of the legend
-  lisi + theme(legend.box.margin = margin(0, 0, 0, 12))
-)
-
-purity <- 1; balance <- 3; mixing <- 3
+purity <- 1; balance <- 1; mixing <- 1
 partition = 'universe'
 method = 'scvIprojection'
 perf %>%  
-  filter(partition %in% c('universe','universeHVG')) %>% 
+  filter(partition %in% c('universe')) %>% 
   select(-clusterN, -clusterMedian, -subset) %>% 
   pivot_wider(names_from = c('Score','Group'), values_from = Value) %>% 
+  select(-LISI_CellType) %>% 
   left_join(accuracy %>% 
-              filter(partition %in% c('universe','universeHVG')) %>% 
-              group_by(norm, nf, dims, method, knn, epochs) %>% summarise(xgboost_score = mean(score))) %>% 
-  mutate(sumZScale =
-           -scale(LISI_CellType)[,1] * purity +
-           scale(Silhouette_CellType)[,1] * purity  +
-           scale(Silhouette_Cluster)[,1] * purity  +
-           -scale(LISI_Cluster)[,1] * purity  +
+              filter(partition %in% c('universe')) %>% 
+              group_by(norm, nf, dims, method, knn, epochs) %>% 
+              summarise(bad_score_sum = sum(score < 0.8), xgboost_score = mean(score))) %>% 
+  left_join(perf_CT %>% 
+              bind_rows() %>% 
+              filter(Score == 'Silhouette') %>% 
+              group_by(normalization, nf, dims, method, knn, epochs) %>% 
+              summarise(Silhouette_CellType = mean(Value, na.rm = TRUE)) 
+              ) %>% 
+  left_join(perf_CT %>% 
+              bind_rows() %>% 
+              filter(Score == 'LISI') %>% 
+              group_by(normalization, nf, dims, method, knn, epochs) %>% 
+              summarise(LISI_CellType = mean(Value, na.rm = TRUE)) 
+  ) %>% 
+  mutate(Score =
+           scales::rescale(LISI_CellType, c(1,0)) * purity +
+           scales::rescale(Silhouette_CellType, c(0,1)) * purity  +
+           scales::rescale(Silhouette_Cluster, c(0,1)) * purity  +
+           scales::rescale(LISI_Cluster, c(1,0)) * purity  +
            
-           scale(`xgboost_score`)[,1] * balance +
+           scales::rescale(`xgboost_score`, c(0,1)) * balance +
+           scales::rescale(`NMI_CellType-Cluster`, c(0,1)) * balance  +
+           scales::rescale(`ARI_CellType-Cluster`, c(0,1)) * balance  +
            
-           scale(`NMI_CellType-Cluster`)[,1] * balance  +
-           scale(`ARI_CellType-Cluster`)[,1]* balance  +
-           
-           scale(LISI_Batch)[,1] * mixing  + # Z score
-           -scale(Silhouette_Batch)[,1] * mixing) %>% 
+           scales::rescale(LISI_Batch, c(0,1)) * mixing  + # Z score
+           scales::rescale(Silhouette_Batch, c(1,0)) * mixing,
+         Score = Score / 9) %>% 
   mutate(nf = as.factor(nf),
          `scVI latent dims` = as.factor(dims),
          method = gsub('scVIprojection', 'scVI-projection',method),
          method = gsub('scVI$', 'scVI-standard', method),
          method = factor(method, levels = c('scVI-standard','scVI-projection'))) %>% 
-  ggplot(aes(x=sumZScale, y = nf, color = `scVI latent dims`, shape = method)) + 
-  #ggbeeswarm::geom_quasirandom(groupOnX=FALSE) +
-  geom_point() +
+  ggplot(aes(x=Score, y = nf, color = `scVI latent dims`, shape = as.factor(epochs))) + 
+  facet_wrap(~Run) +
+  
+  ggbeeswarm::geom_quasirandom(groupOnX=FALSE, size = 4) +
+  #geom_point() +
   facet_wrap(~set) +
   scale_color_manual(values = pals::brewer.set1(n = 10) %>% unname()) +
   scale_fill_manual(values = c('white','gray')) +
